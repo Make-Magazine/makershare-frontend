760e2ff17084564249749462a61518f5b613401a
Abdulrahman Alhomsi <a.alhomsi@orangestudio.com>
2017-02-06 16:33:15 +0200

    update ng7services

--- src/app/components/general/header/header.component.ts
+++ src/app/components/general/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../d7services/user/user.service';
+import { Router } from "@angular/router";
+
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  isLoggedIn = false;
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit() {
+    this.userService.isLogedIn().subscribe(res => {
+      console.log(res);
+      if(res == true){
+        this.isLoggedIn = true;
+      }
+    });
+  }
+
+  Logout(event){
+    console.log(event);
+    this.userService.logout().subscribe();
+
+    this.router.navigate(['']);
   }
 
 }
