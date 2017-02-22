f93a557469a5d58b7911a730375d07de7a6211fd
yaser.marey <w.nabil@orangestudio.com>
2017-02-22 15:42:12 +0200

    adding ng2-ckeditor

--- src/app/components/project/create-project/create-project/create-project.component.ts
+++ src/app/components/project/create-project/create-project/create-project.component.ts
@@ -5,7 +5,6 @@ import { FormBuilder, Validators , FormControl , FormControlDirective , FormGrou
 @Component({
   selector: 'app-create-project',
   templateUrl: './create-project.component.html',
-  styleUrls: ['./create-project.component.css']
 })
 export class CreateProjectComponent implements OnInit {
   
--- src/app/components/project/create-project/team/team.component.ts
+++ src/app/components/project/create-project/team/team.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
-  styleUrls: ['./team.component.css']
 })
 export class TeamComponent implements OnInit {
 
