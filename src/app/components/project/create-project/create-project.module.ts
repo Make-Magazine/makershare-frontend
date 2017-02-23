74adeb9be3fca37bb8369ae0b78068192b6b94a1
A.Abdelraheem <a.abdelraheem@orangestudio.com>
2017-02-23 16:02:42 +0200

    Added ng2-dnd and applied it

--- src/app/components/project/create-project/create-project.module.ts
+++ src/app/components/project/create-project/create-project.module.ts
@@ -7,6 +7,7 @@ import { TeamComponent } from './team/team.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TagInputModule } from 'ng2-tag-input';
 import { CKEditorModule } from 'ng2-ckeditor';
+import { DndModule } from '../../../../../node_modules/ng2-dnd';
 
 
 @NgModule({
@@ -15,6 +16,8 @@ import { CKEditorModule } from 'ng2-ckeditor';
     ReactiveFormsModule,
     TagInputModule,
     CKEditorModule,
+    DndModule.forRoot(),
+
   ],
   declarations: [CreateProjectComponent, YourStoryComponent, HowToComponent, TeamComponent]
 })
