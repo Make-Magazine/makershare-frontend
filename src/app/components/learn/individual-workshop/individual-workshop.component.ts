21ea905cd41525d8e5819b2298b62fef17cf7e8d
asmaabahaa <a.bahaa@orangestudio.com>
2017-02-26 17:46:34 +0200

    merge conflict

---
+++ src/app/components/learn/individual-workshop/individual-workshop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-individual-workshop',
+  templateUrl: './individual-workshop.component.html',
+  styleUrls: ['./individual-workshop.component.css']
+})
+export class IndividualWorkshopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
