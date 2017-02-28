667ee24455286078c8a98bfa785c782ccdcdbd30
asmaabahaa <a.bahaa@orangestudio.com>
2017-02-28 15:39:46 +0200

    fix routing conflict

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,28 +4,8 @@ import { ExploreComponent } from "./components/explore/explore.component";
 import { ProfileComponent } from "./components/account/profile/profile.component";
 import { LearnComponent } from "./components/learn/learn/learn.component";
 import { IndividualWorkshopComponent } from "./components/learn/individual-workshop/individual-workshop.component";
-
-
 import { ChallengesComponent } from "./components/challenge/challenge.module";
-<<<<<<< HEAD
 import { ChallengeSummaryComponent } from "./components/challenge/challenge.module";
-
-const APP_ROUTES: Routes = [
-   { path: '', component: HomeComponent },
-   { path: 'user', component: ProfileComponent },
-   { path: 'explore', component: ExploreComponent },
-   { path: 'challenges', component: ChallengesComponent },
-
-   { path: 'learn', component: LearnComponent },
-
-    { path: 'challenge-summary', component: ChallengeSummaryComponent },
-
-    // { path: 'user/:id', component: UserComponent, children: USER_ROUTES },
-
-    // { path: '**', redirectTo: '/user/1', pathMatch: 'full' }
-];
-=======
-
 
 const APP_ROUTES: Routes = [
     { path: '', component: HomeComponent },
@@ -39,6 +19,5 @@ const APP_ROUTES: Routes = [
     { path: 'workshop/:nid', component: IndividualWorkshopComponent },
 
 ]
->>>>>>> dd56ea78bacd51c06c00d6bba452b3862dd0460f
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
