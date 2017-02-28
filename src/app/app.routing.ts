3bfe828bb411cda3a7e7e6dc66a1ccdc10905503
Abdulrahman Alhomsi <a.alhomsi@orangestudio.com>
2017-02-28 15:26:08 +0200

    fix app.routing.ts merge conflict

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,7 +4,7 @@ import { ExploreComponent } from "./components/explore/explore.component";
 import { ProfileComponent } from "./components/account/profile/profile.component";
 import { LearnComponent } from "./components/learn/learn/learn.component";
 import { ChallengesComponent } from "./components/challenge/challenge.module";
-<<<<<<< HEAD
+
 import { ChallengeSummaryComponent } from "./components/challenge/challenge.module";
 
 const APP_ROUTES: Routes = [
@@ -12,25 +12,15 @@ const APP_ROUTES: Routes = [
    { path: 'user', component: ProfileComponent },
    { path: 'explore', component: ExploreComponent },
    { path: 'challenges', component: ChallengesComponent },
-
    { path: 'learn', component: LearnComponent },
-
-    { path: 'challenge-summary', component: ChallengeSummaryComponent },
+   { path: 'learn', component: LearnComponent },
+   { path: 'learn/:page', component: LearnComponent },
+   { path: 'challenge-summary', component: ChallengeSummaryComponent },
 
     // { path: 'user/:id', component: UserComponent, children: USER_ROUTES },
 
     // { path: '**', redirectTo: '/user/1', pathMatch: 'full' }
 ];
-=======
 
-const APP_ROUTES: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'user', component: ProfileComponent },
-//    { path: 'explore', component: ExploreComponent },
-    { path: 'challenges', component: ChallengesComponent },
-    { path: 'learn', component: LearnComponent },
-    { path: 'learn/:page', component: LearnComponent },
-]
->>>>>>> dd56ea78bacd51c06c00d6bba452b3862dd0460f
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
