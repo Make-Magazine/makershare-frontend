338ca9572d49921b2b7b9b365b23789ae9c07e3b
a.fawzi <a.fawzi@orangestudio.com>
2017-03-02 18:11:39 +0200

    challenges interface class

---
+++ src/app/components/challenge/challenge-data/discussion/discussion.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-discussion',
+  templateUrl: './discussion.component.html',
+})
+export class DiscussionComponent implements OnInit {
+  constructor() { }
+ 
+  ngOnInit() {
+  }
+
+}
--- src/app/components/challenge/challenge.module.ts
+++ src/app/components/challenge/challenge.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChallengesComponent } from './challenges/challenges.component';
-
 import { ChallengeDataComponent } from './challenge-data/challenge-data.component';
-
 import { ChallengeSummaryComponent } from './challenge-summary/challenge-summary.component';
+
+import { RulesComponent } from './challenge-data/rules/rules.component'
+import { DiscussionComponent } from './challenge-data/discussion/discussion.component';
+import { FollowersComponent } from './challenge-data/followers/followers.component';
+import { SummaryComponent } from './challenge-data/summary/summary.component';
+import { AwardsComponent } from './challenge-data/awards/awards.component'
 
 
 @NgModule({
@@ -13,10 +17,14 @@ import { ChallengeSummaryComponent } from './challenge-summary/challenge-summary
   ],
   declarations: [
     ChallengesComponent,
-
     ChallengeDataComponent,
-
     ChallengeSummaryComponent,
+    FollowersComponent,
+   
+    RulesComponent,
+    DiscussionComponent,
+    SummaryComponent,
+    AwardsComponent,
 
   ]
 })
@@ -24,3 +32,4 @@ export class ChallengeModule { }
 export  {ChallengesComponent};
 export {ChallengeDataComponent};
 export  {ChallengeSummaryComponent};
+
