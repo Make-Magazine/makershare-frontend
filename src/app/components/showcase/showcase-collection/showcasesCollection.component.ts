12d4503db187d94a5b6dd7da47722502634295d8
MOKH <muhammadmokhtar92@gmail.com>
2017-03-06 22:32:06 +0200

    temp

--- src/app/components/showcase/showcase-collection/showcasesCollection.component.ts
+++ src/app/components/showcase/showcase-collection/showcasesCollection.component.ts
@@ -12,9 +12,12 @@ export class ShowcasesCollectionComponent implements OnInit {
   pageNumber = 0;
   showcaseCount=0;
   hideloadmore=false;
+  loadFlag=false;
+  sortFlag=false;
   sortData:ISorting;
   sort_order:string;
   sort_by:string;
+  limit=3;
 
   @Input() sortType:ISorting;
   @Input() pageNo:number;
@@ -39,20 +42,39 @@ export class ShowcasesCollectionComponent implements OnInit {
 
   getShowCases(){
 
-    var sort: string;
     // load the showcases
-    this.viewService.getView('showcases',[['page',this.pageNumber],['sort_by',this.sort_by],['sort_order',this.sort_order]]).subscribe(data => {
+      if(this.loadFlag){
+      this.limit+=3;
+      this.viewService.getView('showcases',[['display_id','services_1'],['limit',this.limit],['sort_by',this.sort_by],['sort_order',this.sort_order]]).subscribe(data => {
       this.showcases = this.showcases.concat(data);
-
       this.loadMoreVisibilty();
     }, err => {
 
     });
+    }else if(this.sortFlag){
+      this.viewService.getView('showcases',[['display_id','services_1'],['limit',this.limit],['sort_by',this.sort_by],['sort_order',this.sort_order]]).subscribe(data => {
+      this.showcases = data;
+      this.loadMoreVisibilty();
+    }, err => {
+
+    });
+    }else{
+      this.viewService.getView('showcases',[['display_id','services_1'],['limit',this.limit],['sort_by',this.sort_by],['sort_order',this.sort_order]]).subscribe(data => {
+      this.showcases = data;
+      this.loadMoreVisibilty();
+    }, err => {
+
+    });
+    }
+    this.loadFlag=false;
+    this.sortFlag=false;
+  
   }
 
   // get more click
 loadmore(){
  this.pageNumber++;
+ this.loadFlag = true;
  this.getShowCases();
 }
 // control load more button
@@ -71,6 +93,7 @@ getSortType(event:any){
     this.sortData = event;
     this.sort_by=this.sortData.sort_by;
     this.sort_order = this.sortData.sort_order;
+    this.sortFlag =true;
     this.getShowCases();
 //console.log(this.getProjects);
 }
