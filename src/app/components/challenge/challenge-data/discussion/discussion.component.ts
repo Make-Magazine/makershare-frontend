6d587468864e8bfd81d668735c70ad14767fb572
a.fawzi <a.fawzi@orangestudio.com>
2017-03-06 15:10:08 +0200

    finish follow button and bookmark button and style rules tab

--- src/app/components/challenge/challenge-data/discussion/discussion.component.ts
+++ src/app/components/challenge/challenge-data/discussion/discussion.component.ts
@@ -1,13 +1,37 @@
 import { Component, OnInit,Input } from '@angular/core';
+import { CommentService } from '../../../../d7services/comment/comment.service';
+import { Observable } from "rxjs";
+import { UserService } from '../../../../d7services/user/user.service';
+import { ViewService } from '../../../../d7services/view/view.service'
 
 @Component({
   selector: 'app-discussion',
   templateUrl: './discussion.component.html',
+  providers:[CommentService]
 })
 export class DiscussionComponent implements OnInit {
-  constructor() { }
+  @Input() challenge; 
+   constructor(
+    private nodeComment:CommentService,
+    private viewService:ViewService,
+    private userService:UserService
+  ){}
+  CollabNid;
+ CommenterNid;
+ comments;
+ collabs = [];
+ obvfn;
  
   ngOnInit() {
+     this.CollabNid = this.challenge.nid
+    // console.log(this.CollabNid);
+         this.nodeComment.getNodeCommentsById(this.CollabNid).subscribe(data => {
+         this.comments = data;
+  
+        console.log(this.comments);
+    });
+
   }
+
 
 }
