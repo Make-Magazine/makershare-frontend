bf38a6ac05ffb2bc4241cbcd7640a4d5a2d44681
MOKH <muhammadmokhtar92@gmail.com>
2017-03-07 09:18:55 +0200

    fixes

--- src/app/components/showcase/showcase-collection/showcasesCollection.component.ts
+++ src/app/components/showcase/showcase-collection/showcasesCollection.component.ts
@@ -9,18 +9,15 @@ import { ISorting } from '../../../models/challenge/sorting';
 })
 export class ShowcasesCollectionComponent implements OnInit {
   showcases = [];
-  pageNumber = 0;
   showcaseCount=0;
   hideloadmore=false;
   loadFlag=false;
-  sortFlag=false;
   sortData:ISorting;
   sort_order:string;
   sort_by:string;
   limit=3;
 
   @Input() sortType:ISorting;
-  @Input() pageNo:number;
 
   constructor(
     private viewService: ViewService,
@@ -41,47 +38,34 @@ export class ShowcasesCollectionComponent implements OnInit {
   }
 
   getShowCases(){
-
     // load the showcases
       if(this.loadFlag){
       this.limit+=3;
-      this.viewService.getView('showcases',[['display_id','services_1'],['limit',this.limit],['sort_by',this.sort_by],['sort_order',this.sort_order]]).subscribe(data => {
-      this.showcases = this.showcases.concat(data);
-      this.loadMoreVisibilty();
-    }, err => {
-
-    });
-    }else if(this.sortFlag){
-      this.viewService.getView('showcases',[['display_id','services_1'],['limit',this.limit],['sort_by',this.sort_by],['sort_order',this.sort_order]]).subscribe(data => {
-      this.showcases = data;
-      this.loadMoreVisibilty();
-    }, err => {
-
-    });
-    }else{
-      this.viewService.getView('showcases',[['display_id','services_1'],['limit',this.limit],['sort_by',this.sort_by],['sort_order',this.sort_order]]).subscribe(data => {
-      this.showcases = data;
-      this.loadMoreVisibilty();
-    }, err => {
-
-    });
     }
+      this.viewService.getView('views/showcases',[['display_id','services_1'],['limit',this.limit],['sort_by',this.sort_by],['sort_order',this.sort_order]]).subscribe(data => {
+      this.showcases = data;
+    }, err => {
+
+    });
+    
+    
+    this.loadMoreVisibilty();
     this.loadFlag=false;
-    this.sortFlag=false;
-  
   }
 
   // get more click
 loadmore(){
- this.pageNumber++;
  this.loadFlag = true;
  this.getShowCases();
 }
 // control load more button
 loadMoreVisibilty(){
  // get the challenges array count
- if(this.showcases.length == this.showcaseCount){
+ debugger
+ if(this.showcases.length == this.showcaseCount-1){
+   console.log("flage");
     this.hideloadmore= true;
+    console.log(this.hideloadmore);
  }else{
     this.hideloadmore= false;
  }
@@ -93,15 +77,10 @@ getSortType(event:any){
     this.sortData = event;
     this.sort_by=this.sortData.sort_by;
     this.sort_order = this.sortData.sort_order;
-    this.sortFlag =true;
     this.getShowCases();
 //console.log(this.getProjects);
 }
 
-getPageNumber(event:any){
-this.pageNo = event
-// console.log(this.pageNo);
-}
 
 // get the count of showcases
   showcasesCount() {
