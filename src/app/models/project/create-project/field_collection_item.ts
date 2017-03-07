a5bb12a4d6bc6d9039cdd48396873a4e08c65ce6
yaser.marey <w.nabil@orangestudio.com>
2017-03-07 18:27:53 +0200

    create project base structure for tools

--- src/app/models/project/create-project/field_collection_item.ts
+++ src/app/models/project/create-project/field_collection_item.ts
@@ -1,13 +1,24 @@
 export interface field_collection_item_tool
 {
-  field_tool_name:{und:[field_coolection_item_tool_name]};
-  field_sort_order:{und:[field_coolection_item_tool_sortorder]};
+  field_tool_name:{und:[field_coolection_item_reference_entity]};
+  field_sort_order:{und:[field_coolection_item_reference_number]};
+  field_tool_url :{und:[field_coolection_item_reference_url]};
+  field_description:{und:[field_coolection_item_reference_text]};
+  field_material_quantity:{und:[field_coolection_item_reference_text]};
 }
 
-interface field_coolection_item_tool_name{
+interface field_coolection_item_reference_entity{
   target_id:string;
 }
 
-interface field_coolection_item_tool_sortorder{
+interface field_coolection_item_reference_number{
   value:number;
+}
+
+interface field_coolection_item_reference_text{
+  value:string;
+}
+
+interface field_coolection_item_reference_url{
+  url:URL;
 }
