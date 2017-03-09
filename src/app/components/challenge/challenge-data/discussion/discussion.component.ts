432eda8210a13a65f1319b41b8e11b06d92d9619
a.fawzi <a.fawzi@orangestudio.com>
2017-03-09 13:17:35 +0200

    pull

--- src/app/components/challenge/challenge-data/discussion/discussion.component.ts
+++ src/app/components/challenge/challenge-data/discussion/discussion.component.ts
@@ -21,79 +21,96 @@ export class DiscussionComponent implements OnInit {
     private userService:UserService,
   ){}
 
-newcomment;
-  CollabNid;
-  CommenterNid;
-  comments;
-  collabs = [];
-  obvfn;
-  commentForm: FormGroup;
-  commentData:IComment = {
-    subject: '',
-    comment_body: {und:[{value:''}]},
-    nid: 0,
-  };
+  newcomment;
+      CollabNid;
+      CommenterNid;
+      comments;
+      collabs = [];
+      obvfn;
+      currentuser;
+      commentForm: FormGroup;
+      commentData:IComment = {
+      subject: '',
+      comment_body: {und:[{value:''}]},
+      nid: 0,
+    };
 
 
-  ngOnInit() {
-     
-    this.CollabNid = this.challenge.nid
-    this.commentService.getNodeCommentsById(this.CollabNid).subscribe(data => {
-      this.comments = data;
-      console.log(this.challenge.discussion);
-    });
+          ngOnInit() {
+            
+              this.CollabNid = this.challenge.nid
+              /*  this.commentService.getNodeCommentsById(this.CollabNid).subscribe(data => {
+                  this.comments = data;
+                  console.log(this.challenge.discussion);
+                });
+            */
+              /* var comment = {
+                  "subject":"liJmJfpqMX",
+                  "comment_body":{"und":[{"value":"9DyaNZi3lA"}]},
+                  "nid": "16"
+                }*/
+             // this.onSubmit(event);
+                this.getCurrentUser();
+                this.buildForm();
+                this.getcomments();
+               
+          }
 
-   
+          /* function build form */
+          buildForm (){
+            this.commentForm = this.fb.group({
+              "subject": ['', Validators.required],
+              "comment_body": ['', Validators.required],
+            });
 
-   /* var comment = {
-      "subject":"liJmJfpqMX",
-      "comment_body":{"und":[{"value":"9DyaNZi3lA"}]},
-      "nid": "16"
-    }*/
+          }
+          /* end function build form */
+
+       /* function on submit post comment */
+         onSubmit(e) {
+                  if(this.commentForm.valid){ 
+                  
+                  e.preventDefault();
+                  var body_value = {
+                    }
+                  this.commentData.subject = this.commentForm.value.subject; 
+                  this.commentData.comment_body.und[0].value = this.commentForm.value.comment_body;
+                  this.commentData.nid = this.CollabNid;
+                  this.commentService.createComment(this.commentData).subscribe( res =>{
+                //  this.newcomment = this.newcomment.comment_body.und[0].value;
+                  //console.log(this.commentData.comment_body.und[1].value);/* commnet value */
+                  console.log( this.commentData);
+                  console.log(this.commentData.subject)
+                  this.getcomments();
+                  }, err => {
+                  // console.log(err);
+                  });
+                }
+                this.commentForm.reset();
+             }
+
+         /* end function on submit post comment */
+
+        /* function get comments */
+          getcomments(){
+            this.viewService.getView('node-comments',[['nid',this.CollabNid]]).subscribe(data => {
+            this.comments  =  data;
+            console.log(this.comments);
+            //this.comments  =  this.comments.concat(this.newcomment);
+            //console.log(this.comments[1].comment);  
+          });
+        }
+         /* end function  get comments */
+
+          /* function get current user */
+                getCurrentUser(){
+                    this.userService.getStatus().subscribe(data => {
+                    this.currentuser = data;
+                    console.log(this.currentuser.user.name);
+                  });
+                }
+          /* end function get user*/
 
 
-  
-    this.buildForm();
-    this.getcomments();
-  }
 
-
-  buildForm (){
-    this.commentForm = this.fb.group({
-      "subject": ['', Validators.required],
-      "comment_body": ['', Validators.required],
-    });
-
-  }
-
-  onSubmit(e) {
-    if(this.commentForm.valid){ 
-       
-      e.preventDefault();
-      var body_value = {
-
-      }
-      this.getcomments();
-      this.commentData.subject = this.commentForm.value.subject; 
-      this.commentData.comment_body.und[0].value = this.commentForm.value.comment_body;
-      this.commentData.nid = this.CollabNid;
-      this.commentService.createComment(this.commentData).subscribe( res =>{
-      // this.newcomment = this.commentData;
-       // console.log(this.newcomment.comment_body.und[0].value);/* commnet value */
-      console.log( this.comments.comment_body);
-      }, err => {
-       // console.log(err);
-      });
     }
-    this.commentForm.reset();
-  }
-    getcomments(){
-       this.viewService.getView('node-comments',[['nid',this.CollabNid]]).subscribe(data => {
-      this.comments  =  data;
-      //console.log(this.comments[1].comment);  
-    });
-  }
-
-
-
-}
