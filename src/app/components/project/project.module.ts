0e9324f3a7151a450cd012f73916938c2753617f
A.Abdelraheem <a.abdelraheem@orangestudio.com>
2017-03-09 16:41:16 +0200

    Feedback working on projects

--- src/app/components/project/project.module.ts
+++ src/app/components/project/project.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ProjectRoutingModule } from './project-routing.module';
 import { CreateProjectModule } from './create-project/create-project.module';
 import { ProjectDetailsComponent } from './project-details/project-details.component';
@@ -11,6 +12,7 @@ import { ProjectHowToComponent } from './project-details/project-how-to/project-
     CommonModule,
     CreateProjectModule,
     ProjectRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [ProjectDetailsComponent, ProjectStoryComponent, ProjectHowToComponent],
   exports:[]
