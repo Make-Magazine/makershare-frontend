238ec0e00b2b018a6631b04eb7bbe3951ca70215
youssef <y.salah@orangestudio.com>
2017-03-11 16:19:44 +0200

    delete one messsage

--- src/app/components/account/messages/messages.module.ts
+++ src/app/components/account/messages/messages.module.ts
@@ -5,13 +5,15 @@ import { MessagesRoutsModule } from './messages.routing.module';
 import { ViewComponent } from './view/view.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+
  
 @NgModule({
   imports: [
     CommonModule,
     MessagesRoutsModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+
   ],
   declarations: [
     InboxComponent,
