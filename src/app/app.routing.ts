f4ed0f3084db8a410270d2e2f6c648b6650a7043
yaser.marey <w.nabil@orangestudio.com>
2017-03-14 14:34:43 +0200

    check project status before submit

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,4 @@
 import { RouterModule, Routes } from "@angular/router";
-// import { ProfileComponent } from "./components/account/profile/view/profile.component";
-// import { AllProfileComponent } from './components/account/profile/edit/all-profile/all-profile.component';
-// import { ChallengeProjectComponent } from "./components/challenge/enter-challenge-project/challenge-project.component"
-// import { IndividualWorkshopComponent } from "./components/learn/individual-workshop/individual-workshop.component";
-// import { ShowcasesCollectionComponent, SinglShowcaseComponent } from "./components/showcase/showcases.module";
-// import { LearnComponent } from "./components/learn/learn/learn.component";
-// import { ChallengesComponent, ChallengeDataComponent } from "./components/challenge/challenge.module";
-// import { HomeComponent } from "./components/home/home.component";
-// import { ExploreModule } from "./components/explore/explore.module";
 
 
 const APP_ROUTES: Routes = [
@@ -18,12 +9,6 @@ const APP_ROUTES: Routes = [
     { path: 'learn', loadChildren: 'app/components/learn/learn.module#LearnModule' },
     { path: 'challenges', loadChildren: 'app/components/challenge/challenge.module#ChallengeModule' },
     { path: 'profile', loadChildren: 'app/components/account/profile/profile.module#ProfileModule' },
-    // { path: 'user', component: ProfileComponent },
-    // { path: 'challenge-data/:nid', component: ChallengeDataComponent },
-    // { path: 'enter-challenge/:nid', component: ChallengeProjectComponent },
-    // { path: 'learn/:page', component: LearnComponent },
-    // { path: 'workshop/:nid', component: IndividualWorkshopComponent },
-    // { path: 'single-showcase/:nid', component: SinglShowcaseComponent },
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
