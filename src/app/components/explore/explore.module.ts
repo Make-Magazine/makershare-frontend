9582f50e7dfd3acc04447f457614e082a347200e
a.fawzi <a.fawzi@orangestudio.com>
2017-03-14 15:21:39 +0200

    user card

--- src/app/components/explore/explore.module.ts
+++ src/app/components/explore/explore.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExploreComponent } from './explore.component';
 import { ExploreRoutingModule } from './explore-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
 
 @NgModule({
   imports: [
     CommonModule,
-    ExploreRoutingModule
+    ExploreRoutingModule,
+    SharedModule
   ],
   declarations: [ExploreComponent],
   exports:[]
