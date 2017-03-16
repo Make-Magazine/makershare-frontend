0e990032e7cc027cda1484a862fe87268da31531
youssef <y.salah@orangestudio.com>
2017-03-16 14:20:11 +0200

    reply for message

--- src/app/components/account/messages/view/reply.ts
+++ src/app/components/account/messages/view/reply.ts
@@ -1,4 +1,5 @@
 export interface Reply {
     thread_id: number,
     body: string,
+
 }
--- src/app/components/account/messages/view/view.component.ts
+++ src/app/components/account/messages/view/view.component.ts
@@ -5,6 +5,7 @@ import { UserService } from '../../../../d7services/user/user.service';
 import { Observable } from 'rxjs/Observable'
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { Reply } from './reply';
+import {Location} from '@angular/common';
 
 
 @Component({
@@ -15,10 +16,11 @@ export class ViewComponent implements OnInit {
   msg;
   user = [];
   messages = [];
+  currentuser;
   messageForm: FormGroup;
   reply: Reply = {
     thread_id:0,
-    body: '',
+    body: ''
   };
   constructor(
     private route: ActivatedRoute,
@@ -26,6 +28,7 @@ export class ViewComponent implements OnInit {
     private router: Router,
     private userService: UserService,
     private fb: FormBuilder,
+    private _location: Location,
     
   ) { }
 
@@ -33,6 +36,7 @@ export class ViewComponent implements OnInit {
     this.buildForm();
     this.getThreads();
     var thread_id;
+    this. getCurrentUser();
   }
   getThreads(){
     this.route.params
@@ -44,24 +48,34 @@ export class ViewComponent implements OnInit {
           let i = 0
           this.userService.getUser(message.author).subscribe(res => {
             Object.assign(message, res);
-          console.log(message.user_photo)
-            
+            //console.log(message.user_photo)
           })
           i++
         }
       });
   }
-  
+   getCurrentUser() {
+    this.userService.getStatus().subscribe(data => {
+      this.currentuser = data;
+      //console.log(this.currentuser.user.uid);
+      this.userService.getUser(this.currentuser.user.uid).subscribe(res => {
+        Object.assign(this.user, res);
+          })
+    });
+  }
   onSubmit(e) {
     e.preventDefault();
     this.getThreads();
-    if (this.messageForm.valid) {
+    this.getCurrentUser();
+    if (this.messageForm.valid) {        
        this.reply.thread_id = this.msg.pmtid;
        this.reply.body = this.messageForm.value.body;
-      this.pm.sendMessage(this.reply).subscribe(res => {
+       this.pm.sendMessage(this.reply).subscribe(res => {
         var newComment = {
            thread_id: this.reply.thread_id,
-           photo: this.msg.user_photo,
+           user_photo: this.user['user_photo'],
+           first_name: this.user['first_name'],
+           last_name: this.user['last_name'],
            body: this.reply.body
         }
         console.log(newComment)
@@ -105,6 +119,9 @@ export class ViewComponent implements OnInit {
       'required': 'Message Body is required.',
     },
   };
+  previousUrl() {
+     this._location.back();
+  }
 }
 
  
