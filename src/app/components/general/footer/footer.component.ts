375e4949a9734b51904cd4a45753fd6742506eb6
H-alhomsi <h.alhomsi@orangestudio.com>
2017-03-23 15:16:10 +0200

    update challenge enter

--- src/app/components/general/footer/footer.component.ts
+++ src/app/components/general/footer/footer.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
 import { FormGroup, FormControl, FormBuilder, ReactiveFormsModule, FormArray } from '@angular/forms';
 
 @Component({
