a5c4b7709ec3d5eed5f9dd16a4a202ce8ad06820
a.fawzi <a.fawzi@orangestudio.com>
2017-03-28 20:04:51 +0200

    add load more to explore page

--- src/app/components/explore/explore.component.ts
+++ src/app/components/explore/explore.component.ts
@@ -11,6 +11,11 @@ export class ExploreComponent implements OnInit {
   projects = [];
   categories = null;
   view = 'grid';
+  pages: number = 0;
+  countProject=0;
+  hideloadmoreproject = false;
+
+  page_arg;
   constructor(
     private router: Router,
     private viewService: ViewService,
@@ -18,15 +23,8 @@ export class ExploreComponent implements OnInit {
 
   ngOnInit() {
 
-    // get the projects
-    this.viewService.getView('browse_projects').subscribe(data => {
-      // console.log(data);
-      this.projects = data;
-      console.log(this.projects);
-    }, err => {
-
-    });
-
+    this.getProjects();
+    this.getCountProject();
     // get the categories
     // this.viewService.getView('maker_taxonomy_category/2', []).subscribe(data => {
 
@@ -44,6 +42,20 @@ export class ExploreComponent implements OnInit {
 
   }
 
+getProjects(){
+    // get the projects
+     if (this.pages >= 0) {
+      this.page_arg = ['page', this.pages];
+    }
+    this.viewService.getView('browse_projects', [['page', this.pages]]).subscribe(data => {
+      // console.log(data);
+      this.projects = this.projects.concat(data);
+      console.log(this.projects);
+    }, err => {
+
+    });
+
+}
   projectsById(event){
     var id = event.target.id;
     this.viewService.getView('browse_projects', [['category', id],]).subscribe(data => {
@@ -52,7 +64,39 @@ export class ExploreComponent implements OnInit {
 
     });
   }
-  
+  /* function to get count projects */
+  getCountProject(){
+     this.viewService.getView('maker_count_all_projects').subscribe(data => {
+      this.countProject = data[0];
+      console.log(this.countProject);
+    }, err => {
+
+    });
+  }
+  /* end count function */
+  /* function load more  */
+  loadMoreProject() {
+    this.pages++;
+    console.log(this.pages);
+    this.getProjects();
+  }
+  /* end function load more  */
+  // Function to control load more button
+  loadMoreVisibilty() {
+    // get the challenges array count
+    this.getCountProject();
+    // this.getSortType(event);
+    if (this.countProject == this.projects.length) {
+      this.hideloadmoreproject = true;
+
+      // console.log(this.projects.length);
+      //console.log(this.countProjects);
+    } else if (this.countProject > this.projects.length) {
+      setTimeout(10000);
+      this.hideloadmoreproject = false;
+    }
+  }
+  /* END FUNCTION loadMoreVisibilty */
  
 
 }
