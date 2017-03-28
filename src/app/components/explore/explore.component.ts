b0d0152ca44965d24624a84fff05cfd1c7e8e962
a.fawzi <a.fawzi@orangestudio.com>
2017-03-28 21:55:26 +0200

    add recent first filter and delete filter by all project and winner

--- src/app/components/explore/explore.component.ts
+++ src/app/components/explore/explore.component.ts
@@ -14,34 +14,34 @@ export class ExploreComponent implements OnInit {
   categories = null;
   view = 'grid';
   pages: number = 0;
-  countProject=0;
+  countProject = 0;
   hideloadmoreproject = false;
   page_arg;
   sort: ISorting = {
-    sort_by: "title",
-    sort_order: "ASC",
+    sort_by: "created_2",
+    sort_order: "DESC",
     pageNo: 0
   };
-  ActionName="Most Recent";
+  ActionName = "Most Recent";
   constructor(
     private router: Router,
     private viewService: ViewService,
   ) { }
 
   ngOnInit() {
-     this.getProjects();
+    this.getProjects();
     this.getCountProject();
   }
 
-getProjects(){
+  getProjects() {
     // get the projects
-     if (this.pages >= 0) {
+    if (this.pages >= 0) {
       this.page_arg = ['page', this.pages];
     }
-    if(this.pages == 0) {
+    if (this.pages == 0) {
       this.projects = [];
     }
-    this.viewService.getView('browse_projects', [['page', this.pages],['sort_by', this.sort.sort_by],['sort_order', this.sort.sort_order]]).subscribe(data => {
+    this.viewService.getView('browse_projects', [['page', this.pages], ['sort_by', this.sort.sort_by], ['sort_order', this.sort.sort_order]]).subscribe(data => {
       // console.log(data);
       this.projects = this.projects.concat(data);
       // console.log(this.projects);
@@ -49,8 +49,8 @@ getProjects(){
 
     });
 
-}
-  projectsById(event){
+  }
+  projectsById(event) {
     var id = event.target.id;
     this.viewService.getView('browse_projects', [['category', id],]).subscribe(data => {
       this.projects = data;
@@ -59,8 +59,8 @@ getProjects(){
     });
   }
   /* function to get count projects */
-  getCountProject(){
-     this.viewService.getView('maker_count_all_projects').subscribe(data => {
+  getCountProject() {
+    this.viewService.getView('maker_count_all_projects').subscribe(data => {
       this.countProject = data[0];
       // console.log(this.countProject);
     }, err => {
@@ -71,7 +71,6 @@ getProjects(){
   /* function load more  */
   loadMoreProject() {
     this.pages++;
-    // console.log(this.pages);
     this.getProjects();
   }
   /* end function load more  */
@@ -79,21 +78,18 @@ getProjects(){
   loadMoreVisibilty() {
     // get the challenges array count
     this.getCountProject();
-    // this.getSortType(event);
     if (this.countProject == this.projects.length) {
       this.hideloadmoreproject = true;
 
-      // console.log(this.projects.length);
-      //console.log(this.countProjects);
     } else if (this.countProject > this.projects.length) {
       setTimeout(10000);
       this.hideloadmoreproject = false;
     }
   }
   /* END FUNCTION loadMoreVisibilty */
-    /* function to sort challenge Title A-z */
+  /* function to sort challenge Title A-z */
   sortAsc() {
-
+     this.projects = [];
     this.pages = 0;
     this.sort.sort_order = "ASC";
     this.sort.sort_by = "title";
@@ -104,7 +100,7 @@ getProjects(){
   /* end function to sort challenge Title A-z */
   /* function to sort challenge Title Z-A */
   sortDesc() {
-
+ this.projects = [];
     this.pages = 0
     this.sort.sort_order = "DESC";
     this.sort.sort_by = "title_1"
@@ -114,36 +110,37 @@ getProjects(){
 
   }
   /* end function to sort challenge Title Z-A */
-   /* function to sort challenge Recently */
+  /* function to sort challenge Recently */
   mostRecent() {
-
+ this.projects = [];
     this.pages = 0
     this.sort.sort_order = "DESC"
-    // this.sort.sort_by = "createpageNumberd"
     this.sort.sort_by = "created_2"
-        this.ActionName = "Most Recent"
+    this.ActionName = "Most Recent"
 
     this.getProjects();
 
   }
   /* function to sort challenge Oldest */
   oldest() {
+     this.projects = [];
     this.pages = 0
     this.sort.sort_order = "ASC";
     this.sort.sort_by = "created_1"
-        this.ActionName = "Oldest"
+    this.ActionName = "Oldest"
 
-       this.getProjects();
+    this.getProjects();
 
   }
   /* end function to sort challenge Oldest */
 
   /* function to sort challenge MostLiked */
   mostLiked() {
+     this.projects = [];
     this.pages = 0
     this.sort.sort_order = "DESC";
     this.sort.sort_by = "count"
-        this.ActionName = "Most Liked"
+    this.ActionName = "Most Liked"
 
     this.getProjects();
 
@@ -152,16 +149,17 @@ getProjects(){
 
   /* function to sort challenge MostForked */
   mostForked() {
+     this.projects = [];
     this.pages = 0
     this.sort.sort_order = "DESC";
     this.sort.sort_by = "field_total_forks_value";
-        this.ActionName = "Most Forked"
+    this.ActionName = "Most Forked"
 
     this.getProjects();
 
   }
   /* end function to sort challenge MostLiked */
 
- 
+
 
 }
