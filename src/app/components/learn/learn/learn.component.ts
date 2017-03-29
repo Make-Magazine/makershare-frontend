fd5bffa14b056c41d42becb41181ad125da84ae0
a.fawzi <a.fawzi@orangestudio.com>
2017-03-29 18:27:50 +0200

    add load more to workshop

--- src/app/components/learn/learn/learn.component.ts
+++ src/app/components/learn/learn/learn.component.ts
@@ -8,9 +8,14 @@ import { Router} from '@angular/router';
   styleUrls: ['./learn.component.css']
 })
 export class LearnComponent implements OnInit {
- learns = null;
+ learns = [];
  categories = null;
  workshop;
+pages: number = 0;
+page_arg;
+countWorkshop = 0;
+hideloadmoreworkshop = false;
+
 
   constructor(
     private router: Router,
@@ -18,16 +23,55 @@ export class LearnComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-
+this.getWorkshop();
+  }
+  getWorkshop(){
+    var args = [
+      ['page', this.pages],
+    ];
     // get workshop main page from a view
-    this.viewService.getView('learn', []).subscribe(data => {
-      this.learns = data;
+    this.viewService.getView('learn', args).subscribe(res => {
+    this.learns=this.learns.concat(res);
+    this.loadMoreVisibilty();
+
+
+console.log(this.learns);
+
     }, err => {
 
     });
   }
+   /* function to get count projects */
+  getCountWorkshop() {
+    this.viewService.getView('maker_count_all_workshops/').subscribe(data => {
+      this.countWorkshop = data[0];
+    }, err => {
+
+    });
+  }
+  /* end count function */
     WorkshopDetails(nid) {
     this.router.navigate(['/learn', nid]);
   }
+  loadMoreWorkshop(){
+     this.pages++;
+     console.log(this.pages);
+     this.getWorkshop();
+
+  }
+  // Function to control load more button
+  loadMoreVisibilty() {
+    // get the challenges array count
+    this.getCountWorkshop();
+    if (this.countWorkshop == this.learns.length) {
+      this.hideloadmoreworkshop = true;
+
+    } else if (this.countWorkshop > this.learns.length) {
+      setTimeout(10000);
+
+      this.hideloadmoreworkshop = false;
+    }
+  }
+  /* END FUNCTION loadMoreVisibilty */
 
 }
