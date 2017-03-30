d72ab3ea3e9c7e1a220b780c2fea9e44c4055879
Abdulrahman Alhomsi <a.alhomsi@orangestudio.com>
2017-03-30 22:25:46 +0200

    modify examples.globals.ts

--- src/app/d7services/example.globals.ts
+++ src/app/d7services/example.globals.ts
@@ -1,6 +1,6 @@
 'use strict';
 
 export var domain = 'http://makerdev.orangestudio.com:8080';
-export var solrPath = 'http://localhost:8983/solr/drupal/';
+export var solrPath = 'http://makerdev.orangestudio.com:8389/solr/drupal/';
 export var endpoint = '/api';
 export var appName = 'Maker Share';
