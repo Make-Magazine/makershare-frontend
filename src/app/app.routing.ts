3c2038432113e6ebe5dc38b918a44434cda723cf
a.fawzi <a.fawzi@orangestudio.com>
2017-04-03 16:22:43 +0200

    add canactive authentication

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -11,7 +11,7 @@ const APP_ROUTES: Routes = [
     { path: 'explore', loadChildren: 'app/components/explore/explore.module#ExploreModule' },
     { path: 'learn', loadChildren: 'app/components/learn/learn.module#LearnModule' },
     { path: 'challenges', loadChildren: 'app/components/challenge/challenge.module#ChallengeModule' },
-    { path: 'profile', loadChildren: 'app/components/account/profile/profile.module#ProfileModule', canActivate: [AuthGuardService] },
+    { path: 'profile', loadChildren: 'app/components/account/profile/profile.module#ProfileModule' },
     { path: 'search', loadChildren: 'app/components/search/search.module#SearchModule' },
     { path: 'access-denied', component:  AccessDeniedComponent},
     { path: '**', component: Four04Component }
--- src/app/components/account/messages/messages.routing.module.ts
+++ src/app/components/account/messages/messages.routing.module.ts
@@ -3,14 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { InboxComponent } from './inbox/inbox.component';
 import { ViewComponent } from './view/view.component';
 import { BlockedUsersComponent } from './blocked-users/blocked-users.component';
+import { AuthGuardService } from '../../../auth0/auth-guard.service';
+import { AccessDeniedComponent } from '../../../auth0/access-denied/access-denied.component';
+import { Four04Component } from '../../../auth0/four04/four04.component';
 
 
 
 
 const MessagesRouts: Routes = [
-  { path: 'inbox',  component:  InboxComponent},
-   { path: 'view/:thread_id', component:  ViewComponent},
-   { path: 'blocked', component:  BlockedUsersComponent}
+  { path: 'inbox',  component:  InboxComponent,  canActivate: [AuthGuardService]},
+   { path: 'view/:thread_id', component:  ViewComponent,  canActivate: [AuthGuardService]},
+   { path: 'blocked', component:  BlockedUsersComponent,  canActivate: [AuthGuardService]}
 ];
 
 @NgModule({
