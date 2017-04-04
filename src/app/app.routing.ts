3f871f5a59bb21090d1266f7cf083d99f5496cbb
a.fawzi <a.fawzi@orangestudio.com>
2017-04-04 21:08:38 +0200

    add pages to app

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,14 @@ import { RouterModule, Routes } from "@angular/router";
 import { AuthGuardService } from './auth0/auth-guard.service';
 import { AccessDeniedComponent } from './auth0/access-denied/access-denied.component';
 import { Four04Component } from './auth0/four04/four04.component';
+// static pages
+import { MakerMovementComponent } from './components/pages/maker-movement/maker-movement.component';
+import { IntelMakeComponent } from './components/pages/intel-make/intel-make.component';
+import { AboutBadgesComponent } from './components/pages/about-badges/about-badges.component';
+import { TermsComponent } from './components/pages/terms/terms.component';
+import { OtherSitesComponent } from './components/pages/other-sites/other-sites.component';
+import { MakezineComponent } from './components/pages/makezine/makezine.component';
+import { MakerFaireComponent } from './components/pages/maker-faire/maker-faire.component';
 
 
 const APP_ROUTES: Routes = [
@@ -14,7 +22,16 @@ const APP_ROUTES: Routes = [
     { path: 'profile', loadChildren: 'app/components/account/profile/profile.module#ProfileModule' },
     { path: 'search', loadChildren: 'app/components/search/search.module#SearchModule' },
     { path: 'access-denied', component:  AccessDeniedComponent},
-    { path: '**', component: Four04Component }
+    { path: '**', component: Four04Component },
+    // static pages
+    { path: 'maker-movement', component: MakerMovementComponent},
+    { path: 'intel-make', component: IntelMakeComponent},
+    { path: 'about-badges', component: AboutBadgesComponent},
+    { path: 'terms', component: TermsComponent},
+    { path: 'other-sites', component: OtherSitesComponent},
+    { path: 'makezine', component: MakezineComponent},
+    { path: 'maker-faire', component: MakerFaireComponent},
+
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
