ce17b31566038de53b8b7f2ebe5640b27e075260
a.fawzi <a.fawzi@orangestudio.com>
2017-04-04 21:22:16 +0200

    add components tp pages

---
+++ src/app/components/pages/intel-innovation/intel-innovation.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intel-innovation',
+  templateUrl: './intel-innovation.component.html',
+})
+export class IntelInnovationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/pages/maker-intel/maker-intel.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maker-intel',
+  templateUrl: './maker-intel.component.html',
+})
+export class MakerIntelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
