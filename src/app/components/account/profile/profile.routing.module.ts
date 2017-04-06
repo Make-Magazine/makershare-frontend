08f8fd9c92080d183d8fa308f349a465b74436bd
MOKH <muhammadmokhtar92@gmail.com>
2017-04-06 16:25:04 +0200

    access profiles with username 'profile/user_name'

--- src/app/components/account/profile/profile.routing.module.ts
+++ src/app/components/account/profile/profile.routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { EditProfileComponent } from './edit/edit-profile/edit-profile.component';
 import { AllProfileComponent } from './edit/all-profile/all-profile.component';
 import { ProfileComponent } from './view/profile.component'
+import { ProfileComponent as profilecomponent} from './view/profile.component';
 import { AuthGuardService } from '../../../auth0/auth-guard.service';
 import { AccessDeniedComponent } from '../../../auth0/access-denied/access-denied.component';
 import { Four04Component } from '../../../auth0/four04/four04.component'
@@ -14,7 +15,8 @@ const ProfileRouts: Routes = [
   {
     path: '',
     children: [
-      { path: '', component: ProfileComponent },
+      { path: '',  component: ProfileComponent},
+      { path: ':user_name', component: profilecomponent },
       { path: 'editprofile', loadChildren: 'app/components/account/profile/edit/edit-profile/edit-profile.module#EditProfileModule',  canActivate: [AuthGuardService]},
       { path: 'editportfolio', loadChildren: 'app/components/account/profile/edit/edit-portfolio/edit-portfolio.module#EditPortfolioModule',  canActivate: [AuthGuardService]},
       { path: 'collection', loadChildren: 'app/components/account/collection/collection.module#CollectionModule',  canActivate: [AuthGuardService]},
