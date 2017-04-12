f933141b0c4034daf03edbf9f829d779fdafed9a
a.fawzi <a.fawzi@orangestudio.com>
2017-04-12 16:41:32 +0200

    delete grid

--- src/app/components/challenge/challenge-data/challenge-data.component.ts
+++ src/app/components/challenge/challenge-data/challenge-data.component.ts
@@ -290,9 +290,10 @@ export class ChallengeDataComponent implements OnInit {
       .subscribe(data => {
         if (data == null) {
           this.countProjects = 0
+          console.log(data);
         } else {
           this.countProjects = data;
-          // console.log(data[0]);
+           console.log(this.countProjects);
         }
       }, err => {
      //   this.notificationBarService.create({ message: 'Sorry, somthing went wrong, try again later.', type: NotificationType.Error });
