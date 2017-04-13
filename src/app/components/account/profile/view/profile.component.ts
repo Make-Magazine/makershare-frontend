7e59a2f7be8bfcfcad7d818a4305660500c7f40b
yaser.marey <w.nabil@orangestudio.com>
2017-04-14 00:46:14 +0200

    account/editprofile finished -need some actions

--- src/app/components/account/profile/view/profile.component.ts
+++ src/app/components/account/profile/view/profile.component.ts
@@ -1,6 +1,7 @@
 import { field_URL } from '../../../../models/Drupal';
 import { Component, OnInit } from '@angular/core';
 import { UserProfile } from "../../../../models/profile/userprofile";
+import { ProfileSocial } from "../../../../models/profile/ProfileSocial";
 import { FormGroup, FormBuilder, Validators, FormControl,FormArray } from '@angular/forms';
 import { ProfileService } from '../../../../d7services/profile/profile.service';
 import { UserService } from '../../../../d7services/user/user.service';
@@ -138,17 +139,17 @@ export class ProfileComponent implements OnInit {
   buildFormSocial() {
 
     this.FormGroupSocial = this.fb.group({
-      'field_website_or_blog': [this.profile.field_social_accounts.field_website_or_blog, [Validators.pattern(this.regexp)]],
-      'field_additional_site': [this.profile.field_social_accounts.field_additional_site, [Validators.pattern(this.regexp)]],
-      'field_facebook': [this.profile.field_social_accounts.field_facebook, [Validators.pattern(this.regexp)]],
-      'field_instagram': [this.profile.field_social_accounts.field_instagram, [Validators.pattern(this.regexp)]],
-      'field_linkedin': [this.profile.field_social_accounts.field_linkedin, [Validators.pattern(this.regexp)]],
-      'field_twitter': [this.profile.field_social_accounts.field_twitter, [Validators.pattern(this.regexp)]],
-      'field_pinterest': [this.profile.field_social_accounts.field_pinterest, [Validators.pattern(this.regexp)]],
-      'field_youtube': [this.profile.field_social_accounts.field_youtube, [Validators.pattern(this.regexp)]],
-      'field_hackster_io': [this.profile.field_social_accounts.field_hackster_io, [Validators.pattern(this.regexp)]],
-      'field_instructables': [this.profile.field_social_accounts.field_instructables, [Validators.pattern(this.regexp)]],
-      'field_hackday': [this.profile.field_social_accounts.field_hackday, [Validators.pattern(this.regexp)]],
+      'field_website_or_blog': [this.profile.field_social_accounts.field_website_or_blog, [CustomValidators.url]],
+      'field_additional_site': [this.profile.field_social_accounts.field_additional_site, [CustomValidators.url]],
+      'field_facebook': [this.profile.field_social_accounts.field_facebook, [CustomValidators.url]],
+      'field_instagram': [this.profile.field_social_accounts.field_instagram, [CustomValidators.url]],
+      'field_linkedin': [this.profile.field_social_accounts.field_linkedin, [CustomValidators.url]],
+      'field_twitter': [this.profile.field_social_accounts.field_twitter, [CustomValidators.url]],
+      'field_pinterest': [this.profile.field_social_accounts.field_pinterest, [CustomValidators.url]],
+      'field_youtube': [this.profile.field_social_accounts.field_youtube, [CustomValidators.url]],
+      'field_hackster_io': [this.profile.field_social_accounts.field_hackster_io, [CustomValidators.url]],
+      'field_instructables': [this.profile.field_social_accounts.field_instructables, [CustomValidators.url]],
+      'field_hackday': [this.profile.field_social_accounts.field_hackday, [CustomValidators.url]],
       'field_preferred': [this.profile.field_social_accounts.field_preferred],
     });
   }
@@ -173,20 +174,7 @@ export class ProfileComponent implements OnInit {
     nickname: '',
     describe_yourself: '',
     bio: '',
-    field_social_accounts: {
-      field_website_or_blog: '',
-      field_additional_site: '',
-      field_facebook: '',
-      field_instagram: '',
-      field_linkedin: '',
-      field_twitter: '',
-      field_pinterest: '',
-      field_youtube: '',
-      field_hackster_io: '',
-      field_instructables: '',
-      field_hackday: '',
-      field_preferred: ''
-    },
+    field_social_accounts: new ProfileSocial(),
     started_making: '',
     field_add_your_makerspace_s_:[]
   };
