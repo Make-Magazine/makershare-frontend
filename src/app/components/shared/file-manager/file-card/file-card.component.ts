d226f2a286c31f63886e3715cf2041ce95ff07ad
yaser.marey <w.nabil@orangestudio.com>
2017-04-17 20:19:51 +0200

    test

---
+++ src/app/components/shared/file-manager/file-card/file-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'file-card',
+  templateUrl: './file-card.component.html',
+})
+export class FileCardComponent implements OnInit {
+ 
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
