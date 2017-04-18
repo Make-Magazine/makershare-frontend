8b8189e53f45ce322145ab0932f6291cea02976d
youssef <y.salah@orangestudio.com>
2017-04-18 18:08:09 +0200

    add meta in header components

--- src/app/components/pages/terms/terms.component.ts
+++ src/app/components/pages/terms/terms.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewService } from '../../../d7services/view/view.service';
 import { LoaderService } from '../../shared/loader/loader.service';
+ import { MetaService } from '@nglibs/meta';
 
 @Component({
   selector: 'app-about-badges',
@@ -19,13 +20,18 @@ export class TermsComponent implements OnInit {
   constructor(
     private viewService: ViewService,
     private loaderService: LoaderService,    
+    private meta: MetaService
   ) { }
 
   ngOnInit() {
     this.loaderService.display(true);
     this.viewService.getView('pages', [['nid', 798]]).subscribe(data => {
       this.title = data[0].title;
-      this.body = data[0].body;
+      this.body = data[0].body;                            
+
+      this.meta.setTitle(`Maker Share | ${this.title}`);
+      this.meta.setTag('og:image', '/assets/logo.png');
+      this.meta.setTag('og:description', this.body);
       this.loaderService.display(false);
     }, err => {
       console.log(err);
