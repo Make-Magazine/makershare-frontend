10c3494cdd5636ced12626fedd7b286d21b6d598
youssef <y.salah@orangestudio.com>
2017-04-18 16:45:15 +0200

    remove meta data from modules

--- src/app/components/showcase/showcase-project/showcase-project.module.ts
+++ src/app/components/showcase/showcase-project/showcase-project.module.ts
@@ -5,14 +5,14 @@ import { ShowcaseRoutingModule } from '../showcase-routing.module'
 // import {ShowcasesCollectionComponent } from '../showcase-collection/showcasesCollection.component'
 import { ShowcaseProjectComponent } from './showcase-project.component'
 import { SharedModule } from '../../shared/shared.module'
-import { MetaModule } from '@nglibs/meta';
+// import { MetaModule } from '@nglibs/meta';
 @NgModule({
   imports: [
     CommonModule,
     ShowcaseRoutingModule,
     SharedModule,
     // ShowcaseCollectionModule,
-    MetaModule.forRoot(),
+    // MetaModule.forRoot(),
   ],
    declarations: [ShowcaseProjectComponent],
   
