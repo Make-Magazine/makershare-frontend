05603c6f3367a196494d985bbdea51ffc3974242
Abdulrahman Alhomsi <a.alhomsi@orangestudio.com>
2017-04-19 23:01:33 +0200

    fix issues

---
+++ src/app/components/pages/why-portfolio/why-portfolio.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ViewService } from '../../../d7services/view/view.service';
+import { LoaderService } from '../../shared/loader/loader.service';
+import { MetaService } from '@nglibs/meta';
+
+@Component({
+  selector: 'app-why-portfolio',
+  template: `
+    <section class="page-node">
+      <div class="container">
+        <h3 class="page-title" *ngIf="title" [innerHTML]="title"></h3>
+        <div class="page-body" *ngIf="body" [innerHTML]="body"></div>
+      </div>
+    </section>  
+`,
+})
+export class WhyPortfolioComponent implements OnInit {
+
+  title = '';
+  body = ''
+  constructor(
+    private viewService: ViewService,
+    private loaderService: LoaderService,
+    private meta: MetaService
+  ) { }
+
+  ngOnInit() {
+    this.loaderService.display(true);
+    this.viewService.getView('pages', [['nid', 1228]]).subscribe(data => {
+      this.title = data[0].title;
+      this.body = data[0].body;
+
+      this.meta.setTitle(`Maker Share | ${this.title}`);
+      this.meta.setTag('og:image', '/assets/logo.png');
+      this.meta.setTag('og:description', this.body);
+      this.loaderService.display(false);
+    }, err => {
+      console.log(err);
+      this.loaderService.display(false);
+    });
+  }
+
+}
