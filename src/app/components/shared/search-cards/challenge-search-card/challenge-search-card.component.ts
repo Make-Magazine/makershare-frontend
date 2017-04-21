810826a8161c8c8408bf64d670b18bd2a451b039
A.Abdelraheem <a.abdelraheem@orangestudio.com>
2017-04-21 18:41:25 +0200

    shadows and search fixes

--- src/app/components/shared/search-cards/challenge-search-card/challenge-search-card.component.ts
+++ src/app/components/shared/search-cards/challenge-search-card/challenge-search-card.component.ts
@@ -11,15 +11,15 @@ import { UserService } from '../../../../d7services/user/user.service';
   templateUrl: './challenge-search-card.component.html',
 })
 export class ChallengeSearchCardComponent implements OnInit {
-    challenge: IChallengeData = {
+  challenge: IChallengeData = {
     title: "",
     cover_image: "",
     sponsored_by: "",
     public_voting: 0,
     body: "",
     rules: "",
-    diffDays:0,
-    opened:false,
+    diffDays: 0,
+    opened: false,
     display_entries: 0,
     nid: 0,
     challenge_start_date: {
@@ -47,43 +47,43 @@ export class ChallengeSearchCardComponent implements OnInit {
     timezone_db: "",
     date_type: "",
   };
-  loading=true
-challengeData=[];  
- @Input() challengeNid;
-   constructor(private route: ActivatedRoute,
+  loading = true
+  challengeData = [];
+  @Input() challengeNid;
+  constructor(private route: ActivatedRoute,
     private router: Router,
     private viewService: ViewService,
-            private userService: UserService,
+    private userService: UserService,
 
 
-  ){ }
+  ) { }
 
   ngOnInit() {
-   this.getChallenges();
-    }
+    this.getChallenges();
+  }
 
-  getChallenges(){
-     this.viewService.getView('shared-challenge-card',[['nid',this.challengeNid]]).subscribe(data => {
-     this.challenge = data[0];
-           //calculate days difference
-        if (this.challenge) {
-          var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-          var todayDate = new Date();
-          var endDate = new Date(this.challenge.challenge_end_date.value);
-          var diffDays = Math.round(((endDate.getTime() - todayDate.getTime()) / (oneDay)));
-          if (diffDays >= 0) {
-            this.challenge.opened = true;
-            this.challenge.diffDays = diffDays
-          } else {
-            this.challenge.opened = false;
-          }
+  getChallenges() {
+    this.viewService.getView('shared-challenge-card', [['nid', this.challengeNid]]).subscribe(data => {
+      this.challenge = data[0];
+      //calculate days difference
+      if (this.challenge) {
+        var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+        var todayDate = new Date();
+        var endDate = new Date(this.challenge.challenge_end_date.value);
+        var diffDays = Math.round(((endDate.getTime() - todayDate.getTime()) / (oneDay)));
+        if (diffDays >= 0) {
+          this.challenge.opened = true;
+          this.challenge.diffDays = diffDays
+        } else {
+          this.challenge.opened = false;
         }
-         this.challenge.challenge_end_date.value = this.changeDateFormat(this.challenge.challenge_end_date.value);
+        this.challenge.challenge_end_date.value = this.changeDateFormat(this.challenge.challenge_end_date.value);
         this.challenge.challenge_start_date.value = this.changeDateFormat(this.challenge.challenge_start_date.value);
         this.challenge.winners_announcement_date.value = this.changeDateFormat(this.challenge.winners_announcement_date.value);
-  }, err => {
-  });
-}
+      }
+    }, err => {
+    });
+  }
   /* function to change data format */
   changeDateFormat(date) {
     var d;
@@ -98,22 +98,22 @@ challengeData=[];
     return datestring;
   }
   /* end function to change data format */
- /* function to navigate to challenge summary page */
+  /* function to navigate to challenge summary page */
   ShowChallengeDetails(nid) {
     this.router.navigate(['/challenges', nid]);
   }
 
-   enterToChallengeProject(nid) {
-      
-     this.userService.isLogedIn().subscribe(data => {
+  enterToChallengeProject(nid) {
+
+    this.userService.isLogedIn().subscribe(data => {
       //this.checkUserLogin = data;
       if (data == false) {
         //localStorage.setItem('redirectUrl', this.router.url);
         this.router.navigate(['/access-denied']);
-      }else{
-    this.router.navigate(['challenges/enter-challenge', nid]);
+      } else {
+        this.router.navigate(['challenges/enter-challenge', nid]);
       }
-          });
+    });
   }
 
 }
