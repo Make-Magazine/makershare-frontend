6cf3f95549ecfd3fd66df600e45c6922040c7a5a
youssef <y.salah@orangestudio.com>
2017-05-01 16:47:26 +0200

    edit challenge date grammer

--- src/app/components/shared/challenge-card/challenge-card.component.ts
+++ src/app/components/shared/challenge-card/challenge-card.component.ts
@@ -12,6 +12,7 @@ import { UserService } from '../../../d7services/user/user.service';
   templateUrl: './challenge-card.component.html',
 })
 export class ChallengeCardComponent implements OnInit {
+  announce_date;
   countProjects=0;
   challenge: IChallengeData = {
     title: "",
@@ -69,13 +70,17 @@ export class ChallengeCardComponent implements OnInit {
   getChallenges() {
     this.viewService.getView('shared-challenge-card', [['nid', this.challengeNid]]).subscribe(data => {
       this.challenge = data[0];
-      // console.log(data[0]);
       //calculate days difference
       if (this.challenge) {
         var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
         var todayDate = new Date();
         var endDate = new Date(this.challenge.challenge_end_date.value);
         var diffDays = Math.round(((endDate.getTime() - todayDate.getTime()) / (oneDay)));
+
+        var announceDate = new Date(this.challenge.winners_announcement_date.value);
+        var announce = Math.round(((announceDate.getTime() - todayDate.getTime()) / (oneDay)));
+        this.announce_date = announce;
+
         if (diffDays >= 0) {
           this.challenge.opened = true;
           this.challenge.diffDays = diffDays
