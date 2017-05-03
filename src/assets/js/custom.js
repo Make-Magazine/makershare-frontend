29e09a8d0f2222cd5c17d98da29f79bedb88cb4b
A.Abdelraheem <a.abdelraheem@orangestudio.com>
2017-05-03 23:02:51 +0200

    profile edit

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -139,6 +139,17 @@ $(document).ready(function () {
 		$('#search-box-input').focus();
 	})
 	
+	// Toggle profile sidebar view
+	$(document).on('click','.toggleProfile', function(){
+		$(this).children().toggle();
+		$('.hideData').toggle();
+		$('.profile-overlay').toggle();
+		if($('.profile-container').hasClass('indexProfile')){
+				$('.profile-container').removeClass('indexProfile')
+		} else {
+				$('.profile-container').addClass('indexProfile')
+		}
+	})
 	//Index page parallax bg
 	window_width = $(window).width();
 	window_height = $(window).height();
