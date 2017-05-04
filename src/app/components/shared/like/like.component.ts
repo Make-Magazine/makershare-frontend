623582adac28dade7b6277aed2ab94b00614133f
A.Abdelraheem <a.abdelraheem@orangestudio.com>
2017-05-04 21:47:48 +0200

    disabled learn in search page and added coming soon to header, like toggle to unlike

--- src/app/components/shared/like/like.component.ts
+++ src/app/components/shared/like/like.component.ts
@@ -4,10 +4,12 @@ import { ViewService } from '../../../d7services/view/view.service';
 import { FlagService } from '../../../d7services/flag/flag.service';
 import { UserService } from '../../../d7services/user/user.service';
 import { NotificationBarService, NotificationType } from 'angular2-notification-bar/release';
+import { NgbTooltipConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-like',
   templateUrl: './like.component.html',
+  providers: [NgbTooltipConfig],
 
 })
 export class LikeComponent implements OnInit {
@@ -21,7 +23,7 @@ export class LikeComponent implements OnInit {
   countlikes = 0;
   liked = false;
   @Output() countNumber = new EventEmitter<number>();
-
+  like :string;
   isLiked = false;
   constructor(private route: ActivatedRoute,
     private router: Router,
@@ -29,10 +31,15 @@ export class LikeComponent implements OnInit {
     private userService: UserService,
     private flagService: FlagService,
     private notificationBarService: NotificationBarService,
+    private config: NgbTooltipConfig,
 
-  ) { this.router = router; }
+  ) { 
+    this.router = router; 
+    config.placement = 'bottom';
+    config.triggers = 'hover'; 
+  }
   ngOnInit() {
-     this.countLikes();
+    this.countLikes();
     this.userId = localStorage.getItem('user_id');
     this.userService.isLogedIn().subscribe(data => {
       this.checkUserLogin = data;
@@ -40,6 +47,8 @@ export class LikeComponent implements OnInit {
         /*like start */
         this.flagService.isFlagged(this.nodeNid, this.userId, 'like').subscribe(data => {
           this.isLiked = data[0];
+          if(this.isLiked) {this.like = "Unlike this idea";}
+          else {this.like = "Like this idea";}
           // console.log(this.isLiked)
         }, err => {
           //this.notificationBarService.create({ message: 'Sorry, somthing went wrong, try again later.', type: NotificationType.Error});
@@ -49,7 +58,7 @@ export class LikeComponent implements OnInit {
         /*like end*/
       }//end else 
     });//end userservice isLogedIn
-    
+
   }
   countLikes() {
     this.flagService.flagCount(this.nodeNid, 'like').subscribe(response => {
@@ -78,6 +87,7 @@ export class LikeComponent implements OnInit {
           this.isLiked = false;
           this.countlikes--;
           this.countNumber.emit(this.countlikes);
+          this.like = "Like this idea";
         }, err => {
           //this.notificationBarService.create({ message: 'Sorry, somthing went wrong, try again later.', type: NotificationType.Error});
           // console.log(err);
@@ -85,8 +95,9 @@ export class LikeComponent implements OnInit {
       } else {
         this.flagService.flag(this.nodeNid, this.user, 'like').subscribe(response => {
           this.isLiked = true;
-            this.countlikes++;
+          this.countlikes++;
           this.countNumber.emit(this.countlikes);
+          this.like = "Unlike this idea";
         }, err => {
           //this.notificationBarService.create({ message: 'Sorry, somthing went wrong, try again later.', type: NotificationType.Error});
           // console.log(err);
