cb35c5bd4e4f39d5020566f23d9c79b618fe93f7
A.Abdelraheem <a.abdelraheem@orangestudio.com>
2017-05-10 17:57:04 +0200

    profile editing

--- src/app/components/account/profile/view/profile.component.ts
+++ src/app/components/account/profile/view/profile.component.ts
@@ -145,6 +145,16 @@ export class ProfileComponent implements OnInit {
   DefaultView :string = "grid";
   mobileToggle :boolean =false;
   emptySocial :boolean = true;
+  emptySection = {
+      motto: true,
+      bio: true,
+      social: true,
+      badges: true,
+      makerspace:true,
+      websites:true,
+      topics: true,
+      started_making:true,
+    }
   constructor(
     private profileService: ProfileService,
     private userService: UserService,
@@ -444,6 +454,16 @@ export class ProfileComponent implements OnInit {
       if ((social != 'field_website_or_blog' || social != 'field_additional_site') && this.ProfileInfo.field_social_accounts[social]) {
         this.emptySocial = false;
       }
+    }
+
+  }
+  CheckFields(){
+    if(this.uid == this.CurrentLoggedUserId){
+      for(let element in this.emptySection) {
+        this.emptySection[element] = false;
+      }
+    } else {
+     
     }
   }
   ToggleProfile() {
