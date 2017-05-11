08f62db0ddec1384624342ab892586f156f2e909
a.fawzi <a.fawzi@orangestudio.com>
2017-05-11 20:46:23 +0200

    project url from nid to name

--- src/app/components/account/profile/view/edit-portfolio/project-card-portfolio/project-card-portfolio.component.ts
+++ src/app/components/account/profile/view/edit-portfolio/project-card-portfolio/project-card-portfolio.component.ts
@@ -39,6 +39,7 @@ export class ProjectCardPortfolioComponent implements OnInit {
       var membership_array = membership_string.split(',');
       res[0].field_team_members = membership_array;
       this.projectCard = res[0];
+      console.log(this.projectCard)
     });
   }
 
@@ -68,8 +69,8 @@ export class ProjectCardPortfolioComponent implements OnInit {
       this.emitter.emit();
     });
   }
-  ShowProjectDetails(nid) {
-    this.router.navigate(['/projects', nid]);
+  ShowProjectDetails(path) {
+    this.router.navigate(['/projects/', path]);
   }
   getBadgesProject() {
     this.viewService.getView('api-project-badges', [['nid', this.Project.nid]]).subscribe(data => {
