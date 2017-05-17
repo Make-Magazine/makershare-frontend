7c4e7566c8a9dfdbca1f467ebdc47d91aa6d1699
yaser.marey <w.nabil@orangestudio.com>
2017-05-17 15:39:29 +0200

    add manage button to user menu

--- src/app/components/general/header/header.component.ts
+++ src/app/components/general/header/header.component.ts
@@ -4,6 +4,7 @@ import { Router } from "@angular/router";
 import { Auth } from '../../../auth0/auth.service';
 import { SearchBoxComponent } from './search-box/search-box.component';
 import { ProfilePictureService } from '../../shared/profile-picture/profile-picture.service';
+import { domain } from '../../../d7services/globals';
 
 
 
@@ -13,7 +14,9 @@ import { ProfilePictureService } from '../../shared/profile-picture/profile-pict
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  Back_End_Domain;
+  roles = [];
+  Manager:boolean = false;
   showSearchBox: boolean = false;
   user_photo: string;
   displayRegistration:boolean = false;
@@ -26,16 +29,14 @@ export class HeaderComponent implements OnInit {
 
 
   ngOnInit() {
-    // this.userService.isLogedIn().subscribe(res => {
-    //   if (res == true) {
-    //     this.user_photo = localStorage.getItem('user_photo');
-    //   }
-    // });
-    // localStorage.setItem('redirectUrl', this.router.url);
-    // this.user_photo = localStorage.getItem('user_photo');
-    // this.profilePictureService.url.subscribe((val: URL) => {
-    //   this.user_photo = val;
-    // });
+    this.Back_End_Domain = domain;
+    if(localStorage.getItem('roles')){
+      this.roles = localStorage.getItem('roles').split(',');
+      if(this.roles.indexOf('3') != -1 || this.roles.indexOf('4') != -1 || this.roles.indexOf('6') != -1 || this.roles.indexOf('7') != -1 ||
+        this.roles.indexOf('8') != -1 || this.roles.indexOf('9') != -1 || this.roles.indexOf('10') != -1){
+        this.Manager = true;
+      }
+    }
     this.profilePictureService.url.subscribe((val: string) => {
       this.user_photo = val;
     })
