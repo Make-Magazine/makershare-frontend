786ba9be6b8cde2ac82502328aec4223735e4b5b
a.fawzi <a.fawzi@orangestudio.com>
2017-06-08 10:39:22 +0200

    style tooltip likes

--- src/app/components/shared/like/like.component.ts
+++ src/app/components/shared/like/like.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router, RouterModule, ActivatedRoute, Params } from '@angular/router';
-import { ViewService,FlagService,UserService } from '../../../d7services';
+import { ViewService, FlagService, UserService } from '../../../d7services';
 import { NotificationBarService, NotificationType } from 'angular2-notification-bar/release';
 import { NgbTooltipConfig } from '@ng-bootstrap/ng-bootstrap';
 
@@ -16,9 +16,16 @@ export class LikeComponent implements OnInit {
   @Input() showcase = false;
   @Input() project = false;
   userId;
+  hideloadmorelike = true;
+
+  pages: number = 0;
   currentuser;
+  whoLike = [];
+  whoLike2 = [];
+  LoadCount: number;
   checkUserLogin = false;
   countlikes = 0;
+  countLikers: number = 0;
   liked = false;
   @Output() countNumber = new EventEmitter<number>();
   like: string;
@@ -38,12 +45,14 @@ export class LikeComponent implements OnInit {
   }
   ngOnInit() {
     this.countLikes();
+    this.getWhoLike();
+    this.getWhoLike2()
     this.userId = localStorage.getItem('user_id');
     this.userService.isLogedIn().subscribe(data => {
       this.checkUserLogin = data;
       if (data == false) {
 
-       } else {
+      } else {
         /*like start */
         this.flagService.isFlagged(this.nodeNid, this.userId, 'like').subscribe(data => {
           this.isLiked = data[0];
@@ -59,6 +68,24 @@ export class LikeComponent implements OnInit {
       }//end else 
     });//end userservice isLogedIn
 
+  }
+  getWhoLike() {
+    this.viewService.getView('who-liked', [['nid', this.nodeNid]]).subscribe(data => {
+      this.whoLike = data;
+      console.log(this.whoLike)
+      this.LoadCount = this.whoLike.length - 0;
+      // console.log(this.whoLike.length)
+    });
+  }
+  getWhoLike2() {
+    this.viewService.getView('who-liked2', [['nid', this.nodeNid], ['page', this.pages]]).subscribe(data => {
+      if (data[0]) {
+       // console.log(data[0]['likes_count'])
+        this.countLikers = data[0]['likes_count'];
+      }
+      this.whoLike2 = this.whoLike2.concat(data);
+      this.loadMoreVisibilty();
+    });
   }
   countLikes() {
     this.flagService.flagCount(this.nodeNid, 'like').subscribe(response => {
@@ -92,7 +119,7 @@ export class LikeComponent implements OnInit {
           //this.notificationBarService.create({ message: 'Sorry, somthing went wrong, try again later.', type: NotificationType.Error});
           // console.log(err);
         });
-      } else if(!this.isLiked && data == true) {
+      } else if (!this.isLiked && data == true) {
         this.flagService.flag(this.nodeNid, this.user, 'like').subscribe(response => {
           this.isLiked = true;
           this.countlikes++;
@@ -108,4 +135,22 @@ export class LikeComponent implements OnInit {
   }
   /* end function like */
 
+  /* function load more  */
+  loadMoreLike() {
+    this.pages++;
+    this.getWhoLike2();
+  }
+  /* end function load more  */
+  // Function to control load more button
+  loadMoreVisibilty() {
+    // get the challenges array count
+    if (this.countLikers >= this.whoLike2.length) {
+
+      this.hideloadmorelike = true;
+
+    } else if (this.countLikers > this.whoLike2.length) {
+      this.hideloadmorelike = false;
+    }
+  }
+  /* END FUNCTION loadMoreVisibilty */
 }
