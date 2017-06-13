499b702deb2253140bc96e87b52148b1f2f21806
H-alhomsi <h.alhomsi@orangestudio.com>
2017-06-13 17:30:35 +0200

    css update

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -255,7 +255,11 @@ makerShare = {
 				var window_bottom = $(window).scrollTop();
 				var $image = $('.parallax-bg img');
         var $redLines = $('.parallax-red-line');
-				 var oVal = ((window_bottom - parent_top) / 3);
+				var oVal = ((window_bottom - parent_top) / 3);
+				// console.log(parent_top);
+				// console.log(window_bottom);
+				// console.log($image);
+				// console.log($redLines);
         var oVal2 = ((window_bottom - parent_top) / 7);
         $image.css('transform','translate3d(0px, ' + oVal + 'px, 0px)');
         $redLines.each(function() {
