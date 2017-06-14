39743f061b1eda039225b3db2fe91893dbf012e3
H-alhomsi <h.alhomsi@orangestudio.com>
2017-06-14 17:07:54 +0200

    file manager

--- src/app/components/account/profile/view/edit-portfolio/file-manager/file-manager.component.ts
+++ src/app/components/account/profile/view/edit-portfolio/file-manager/file-manager.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
-import { ViewService,FileService } from '../../../../../../d7services';
+import { ViewService, FileService } from '../../../../../../d7services';
 import { FileEntityManage, FileManageService, NodeHelper, FileEntity } from '../../../../../../models';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
@@ -8,70 +8,76 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './file-manager.component.html',
 })
 export class FileManagerComponent implements OnInit {
-  fileManageService:FileManageService;
-  files:FileEntityManage[];
-  ProjectsUsingFile:string[] = [];
-  fileCard:FileEntityManage;
+  fileManageService: FileManageService;
+  files: FileEntityManage[];
+  ProjectsUsingFile: string[] = [];
+  fileCard: FileEntityManage;
 
   constructor(
-    private viewService:ViewService,
-    private fileService:FileService,
-    private modalService:NgbModal
+    private viewService: ViewService,
+    private fileService: FileService,
+    private modalService: NgbModal
   ) { }
 
   ngOnInit() {
     this.UpdateFiles();
   }
 
-  UpdateFiles(){
-    this.viewService.getView('maker_manage_file/'+localStorage.getItem("user_id")).subscribe((data:FileManageService)=>{
+  UpdateFiles() {
+    this.viewService.getView('maker_manage_file/' + localStorage.getItem("user_id")).subscribe((data: FileManageService) => {
       this.fileManageService = data;
+      this.fileManageService.files.forEach(function (file, index) {
+        let fileType = file.filename.split('.');
+        let sourceType = fileType[1];
+        // console.log(sourceType);
+
+      });
       this.files = [];
       this.LoadMore();
-      console.log(this.fileManageService);
+      // console.log(this.fileManageService);
     });
   }
 
-  FileChanged(files:FileList){
+  FileChanged(files: FileList) {
     Array.from(files).forEach(file => {
       this.UploadFile(file);
     });
   }
 
-  UploadFile(file:File){
-    let AllowedFileTypes = ['jpeg','jpg','png','gif','pdf','epub','xls','ppt','pptx','sxls','doc','docx','txt'];
-    if(AllowedFileTypes.indexOf(file.name.split('.')[1]) == -1) return;
+  UploadFile(file: File) {
+    let AllowedFileTypes = ['jpeg', 'jpg', 'png', 'gif', 'pdf', 'epub', 'xls', 'ppt', 'pptx', 'sxls', 'doc', 'docx', 'txt'];
+    if (AllowedFileTypes.indexOf(file.name.split('.')[1]) == -1) return;
     const self = this;
     var myReader: FileReader = new FileReader();
     myReader.onloadend = function (loadEvent: any) {
-      let fileEntity:FileEntity = {
-        file:NodeHelper.RemoveFileTypeFromBase64(loadEvent.target.result),
-        filename:file.name,
-        status:1,
+      let fileEntity: FileEntity = {
+        file: NodeHelper.RemoveFileTypeFromBase64(loadEvent.target.result),
+        filename: file.name,
+        status: 1,
       };
-      self.fileService.SendCreatedFile(fileEntity).subscribe((f)=>{
+      self.fileService.SendCreatedFile(fileEntity).subscribe((f) => {
         self.UpdateFiles();
-      },err=>console.log(err));
+      }, err => console.log(err));
     };
     myReader.readAsDataURL(file);
   }
 
-  LoadMore(){
-    let files = this.fileManageService.files.slice(this.files.length, this.files.length+12);
+  LoadMore() {
+    let files = this.fileManageService.files.slice(this.files.length, this.files.length + 12);
     this.files = this.files.concat(files);
   }
-  CheckFileBeforeDelete(Template,file:FileEntityManage){
+  CheckFileBeforeDelete(Template, file: FileEntityManage) {
     this.ProjectsUsingFile = [];
-    if(file.usage){
+    if (file.usage) {
       this.fileCard = file;
       this.modalService.open(Template);
-    }else{
+    } else {
       this.DeleteFile(file.fid);
     }
   }
 
-  DeleteFile(fid){
-    this.fileService.deleteFile(fid).subscribe((data)=>{
+  DeleteFile(fid) {
+    this.fileService.deleteFile(fid).subscribe((data) => {
       this.UpdateFiles();
     });
   }
