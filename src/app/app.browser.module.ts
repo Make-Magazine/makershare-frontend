44ab36db8327b35da096b34a855a1f6b15771c7e
Abdulrahman Alhomsi <a.alhomsi@orangestudio.com>
2017-06-19 14:09:37 +0200

    new componentm claim your profile

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppModule} from './app.module';
 import { AppComponent } from './app.component';
 
@@ -11,4 +10,4 @@ import { AppComponent } from './app.component';
   ],
   bootstrap: [AppComponent]
 })
-export class AppBrowserModule { }
+export class AppBrowserModule {}
