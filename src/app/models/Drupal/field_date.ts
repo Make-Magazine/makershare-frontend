035f32c91400d4889a351d9aa64eede84423578a
yaser.marey <w.nabil@orangestudio.com>
2017-06-20 13:04:38 +0200

    fix timezone for project edit

--- src/app/models/Drupal/field_date.ts
+++ src/app/models/Drupal/field_date.ts
@@ -13,9 +13,10 @@ export class date_time implements date_time{
       this.date = date_with_time.date;
       this.time = date_with_time.time;
     }else{
-      let now = new Date();
-      let date = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(),  now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
-      date = new Date(date.getTime() + 10*60000);
+      let now = new Date(); //Get current date
+      let date = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(),  now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds()); //reset date to UTC
+      date = new Date(date.getTime() - 3*60*60*1000); //convert to america time zone
+      date = new Date(date.getTime() + 10*60000); // add 10 mins to timeout
       let day = date.getDate();
       let month = date.getMonth()+1;
       let year = date.getFullYear();
