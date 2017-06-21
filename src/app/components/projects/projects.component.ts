b62b8850f9313ef43a195912a6a557c82a6044cb
A.Abdelraheem <a.abdelraheem@orangestudio.com>
2017-06-21 16:55:40 +0200

    Reconstruct Projects Sortings

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -7,6 +7,7 @@ import { LoaderService } from '../shared/loader/loader.service';
 import { MetaService } from '@nglibs/meta';
 import { NotificationBarService, NotificationType } from 'angular2-notification-bar/release';
 import * as globals from '../../d7services/globals';
+import { SortBySortingSet, SortingSet } from '../../models/makers'
 
 
 
@@ -15,6 +16,11 @@ import * as globals from '../../d7services/globals';
   templateUrl: './projects.component.html',
 })
 export class ProjectsComponent implements OnInit {
+  CurrentSortSet: SortingSet = {
+    sort_by: "created_2",
+    sort_order: "DESC",
+  };
+  SortBy: SortBySortingSet = new SortBySortingSet(this.CurrentSortSet, this.viewService)
   projects = [];
   hasContent = false;
   categories = null;
@@ -25,76 +31,12 @@ export class ProjectsComponent implements OnInit {
   hideloadmoreproject = true;
   CurrentActiveParentIndex = -1;
   CurrentActiveChildIndex = -1;
-  page_arg;
-  sort: ISorting = {
-    sort_by: "created_2",
-    sort_order: "DESC",
-    pageNo: 0
-  };
-  ActionName = "Sort";
   categories_parents: ProjectCategory[] = [];
   categories_childs: ProjectCategory[] = [];
   all_categories: ProjectCategory[];
   childCategory = [];
   categoryId;
-  sort_functions = [
-    'dummy',
-    'mostRecent',   
-    'oldest',   
-    'sortAsc',
-    'sortDesc',
-    'mostLiked',   
-    'mostViewed',   
-    'mostFeatured',   
-    'mostForked',   
-    ]
-  sortingSet = {
-    'mostRecent': {
-      'sort_order': "DESC",
-      'sort_by': "created_2",
-      'ActionName': "Most recent",
-    },
-    'oldest': {
-      'sort_order': "ASC",
-      'sort_by': "created_1",
-      'ActionName': "Oldest",
-    },
-    'sortAsc': {
-      'sort_order': "ASC",
-      'sort_by': "title",
-      'ActionName': "Title A-Z",
-    },
-    'sortDesc': {
-      'sort_order': "DESC",
-      'sort_by': "title_1",
-      'ActionName': "Title Z-A",
-    },
-    'mostLiked': {
-      'sort_order': "DESC",
-      'sort_by': "count",
-      'ActionName': "Most liked",
-    },
-    'mostViewed': {
-      'sort_order': "DESC",
-      'sort_by': "php",
-      'ActionName': "Most viewed",
-    },
-    'mostFeatured': {
-      'sort_order': "DESC",
-      'sort_by': "field_projects_target_id",
-      'ActionName': "Featured",
-    },
-    'mostForked': {
-      'sort_order': "DESC",
-      'sort_by': "field_total_forks_value",
-      'ActionName': "Most forked",
-    },
-    'allProject': {
-      'sort_order': "DESC",
-      'sort_by': "created_2",
-      'ActionName': "Most recent",
-    },
-  };
+  
   constructor(
     private router: Router,
     private viewService: ViewService,
@@ -115,35 +57,20 @@ export class ProjectsComponent implements OnInit {
     this.meta.setTag('og:description', 'From 3D printing to robots to yarncraft, browse projects from Maker Faire and all the Maker community. Maker Share is a project by Make: + Intel.');
   }
   getProjects() {
-    // show spinner
     this.loaderService.display(true);
     if (this.pages == 0) {
       this.projects = [];
     }
-    if (this.categoryId) {
-      this.viewService.getView('browse_projects',[['page', this.pages], ['sort_by', this.sort.sort_by], ['sort_order', this.sort.sort_order], ['category', this.categoryId]]).subscribe(data => {
-        this.projects = this.projects.concat(data);
-        this.loadMoreVisibilty();
-        if (this.projects.length == 0) {
-          this.notificationBarService.create({ message: "There aren't any projects yet for this topic. Go make one!", type: NotificationType.Error, allowClose: false, autoHide: true, hideOnHover: false });
-        }
-        // hide spinner
-        this.loaderService.display(false);
-      }, err => {
+    this.SortBy.Sort('browse_projects',this.pages,this.categoryId).subscribe(data => {
+      this.projects = this.projects.concat(data);
+      this.loadMoreVisibilty();
+      if (this.projects.length == 0) {
+        this.notificationBarService.create({ message: "There aren't any projects yet for this topic. Go make one!", type: NotificationType.Error, allowClose: false, autoHide: true, hideOnHover: false });
+      }
+      this.loaderService.display(false);
+    }, err => {
 
-      });
-    } else {
-      this.viewService.getView('browse_projects', [['page', this.pages], ['sort_by', this.sort.sort_by], ['sort_order', this.sort.sort_order]]).subscribe(data => {
-        this.projects = this.projects.concat(data);
-        this.loadMoreVisibilty();
-        // hide spinner
-        this.loaderService.display(false);
-      }, err => {
-        // hide spinner
-        this.loaderService.display(false);
-      });
-    }
-
+    });
   }
   projectsById(event) {
     // show spinner
@@ -188,7 +115,7 @@ export class ProjectsComponent implements OnInit {
       // this.notificationBarService.create({ message: "Sorry, but your project doesn't meet the challenge requirements, Please check <a id='rules-id' href='#rules' data-nodeId='" + this.nid + "'>Rules & Instructions </a>", type: NotificationType.Error, allowClose: true, autoHide: false, hideOnHover: false, isHtml: true });
     });
 
-  }//end function
+  }
   selectParent(value){
     this.CurrentActiveChildIndex = -1;
     this.childCategory = []; 
@@ -205,34 +132,27 @@ export class ProjectsComponent implements OnInit {
       }
     }
   }
-  /* end count function */
-  /* function load more  */
   loadMoreProject() {
     this.pages++;
     this.getProjects();
-  }/* end function load more  */
-  // Function to control load more button
+  }
   loadMoreVisibilty() {
-    // get the challenges array count
     if (this.countProject <= this.projects.length) {
       this.hideloadmoreproject = true;
     } else if (this.countProject > this.projects.length) {
-      //  setTimeout(10000);
       this.hideloadmoreproject = false;
     }
-  }/* END FUNCTION loadMoreVisibilty */
-  /* function to sort challenge Title A-z */
-
-  sortBy(type) {
-    this.projects = [];
-    this.pages = 0;
-    this.sort.sort_order = this.sortingSet[type].sort_order;
-    this.sort.sort_by = this.sortingSet[type].sort_by;
-    this.ActionName = this.sortingSet[type].ActionName;
-    this.getCountProject();    
-    this.getProjects();
   }
-  sortProjects(sort){
-    this.sortBy([this.sort_functions[sort]]);
+
+  sortProjects(sort){   
+    if(sort == '_none') return;
+    this.pages = 0;
+    this.CurrentSortSet.sort_order = "DESC";
+    if( sort == 'created_1' || sort == 'title') {
+      this.CurrentSortSet.sort_order = "ASC";
+    }
+    this.CurrentSortSet.sort_by = sort;
+    this.getCountProject();        
+    this.getProjects();
   }  
 }
