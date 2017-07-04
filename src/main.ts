8fa8dee12049201abe8c284f335238d65c464e52
yaser.marey <w.nabil@orangestudio.com>
2017-07-04 13:22:53 +0200

    remove old universal settings

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import './polyfills.ts';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment.prod';
-import { AppBrowserModule } from './app/app.browser.module';
+import { AppModule } from './app/app.module';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppBrowserModule);
+platformBrowserDynamic().bootstrapModule(AppModule);
