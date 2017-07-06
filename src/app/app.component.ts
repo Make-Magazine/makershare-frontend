d4e15aade980426b056cc2490bd3368f6c415da3
yaser.marey <w.nabil@orangestudio.com>
2017-07-06 18:11:00 +0200

    remove material and copy assets with index and install dependences

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,13 @@
-import {Component, Optional, OnInit, OnDestroy} from '@angular/core';
-
-import {MdDialog, MdDialogRef, MdSnackBar} from '@angular/material';
-
-import {DialogContent} from './dialog-content.component';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css'],
 })
-export class AppComponent implements OnInit, OnDestroy {
-  public isDarkTheme: boolean = false;
-
-  public foods = [
-    {name: 'Pizza', rating: 'Excellent'},
-    {name: 'Burritos', rating: 'Great'},
-    {name: 'French fries', rating: 'Pretty good'},
-  ];
-
-  public progress: number = 0;
-
-  private timer;
-
-  constructor(private dialog: MdDialog, private snackbar: MdSnackBar) {}
+export class AppComponent implements OnInit {
+  constructor() {}
 
   ngOnInit() {
-    const update = () => this.progress = (this.progress + Math.floor(Math.random() * 4) + 1) % 100;
-
-    this.timer = setInterval(() => update, 200);
-  }
-
-  ngOnDestroy() {
-    clearInterval(this.timer);
-  }
-
-  openDialog() {
-    this.dialog.open(DialogContent);
-  }
-
-  showSnackbar() {
-    this.snackbar.open('Yum snacks', 'Chew');
+    
   }
 }
