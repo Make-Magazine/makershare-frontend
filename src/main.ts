631c511fab217637924e97d60784d7c394a179fb
yaser.marey <w.nabil@orangestudio.com>
2017-07-06 14:44:54 +0200

    angular-ssr

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment.prod';
+
+import {prebootClient} from 'preboot/__build/src/browser/preboot_browser';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule).then(() => prebootClient().complete());
