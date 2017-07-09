c55232658a28da9a93fa304f3fda60b87e7bff78
a.fawzi <a.fawzi@orangestudio.com>
2017-07-09 10:50:54 +0200

    load more follow-user

--- src/app/components/shared/follow-user/follow-user.component.ts
+++ src/app/components/shared/follow-user/follow-user.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router, RouterModule, ActivatedRoute, Params } from '@angular/router';
 import { ViewService, FlagService, UserService } from '../../../d7services';
 import { NotificationBarService, NotificationType } from 'angular2-notification-bar/release';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-follow-user',
   templateUrl: './follow-user.component.html',
@@ -13,7 +15,9 @@ export class FollowUserComponent implements OnInit {
     private viewService: ViewService,
     private userService: UserService,
     private flagService: FlagService,
-    private notificationBarService: NotificationBarService, ) { }
+    private notificationBarService: NotificationBarService,
+    private modalService: NgbModal,
+  ) { }
   @Input() userfollower;
   @Output() countNumber = new EventEmitter<number>();
   @Output() countNumberFollowing = new EventEmitter<number>();
@@ -23,6 +27,15 @@ export class FollowUserComponent implements OnInit {
   currentuser;
   CurrentLoggedUserId: number;
   checkUserLogin = false;
+  closeResult: string;
+  whoFollow = [];
+  whoFollowing = [];
+  hideloadmorefollow = true;
+  hideloadmorefollowing = true;
+  pages: number = 0;
+
+
+
   isFollowed;
   ButtonFollow;
   countFollowers = 0;
@@ -74,9 +87,14 @@ export class FollowUserComponent implements OnInit {
     if (userName) {
       this.userService.getIdFromUrl(userName).subscribe(res => {
         this.CurrentLoggedUserId = res.uid;
+        console.log(this.CurrentLoggedUserId)
+        this.getWhoFollow();
+        this.getWhoFollowing();
       }, err => {
       });
     }
+
+
   }
   /* function follow */
   followThis(e: Event) {
@@ -94,6 +112,8 @@ export class FollowUserComponent implements OnInit {
           this.countNumber.emit(this.countFollowers);
           this.HideLoadMore.emit();
           // this.nodeNid = this.nodeNid;
+          this.getWhoFollow();
+          this.getWhoFollowing();
         }, err => {
           //this.notificationBarService.create({ message: 'Sorry, somthing went wrong, try again later.', type: NotificationType.Error});
         });
@@ -105,11 +125,93 @@ export class FollowUserComponent implements OnInit {
           this.countFollowers++;
           this.countNumber.emit(this.countFollowers);
           this.HideLoadMore.emit();
+          this.getWhoFollow();
+          this.getWhoFollowing();
         }, err => {
           //this.notificationBarService.create({ message: 'Sorry, somthing went wrong, try again later.', type: NotificationType.Error});
         });
       }
     });//end if check user login
   }
+
+  follower(contentfollower) {
+
+    this.modalService.open(contentfollower, { size: 'sm' }).result.then((result) => {
+      this.closeResult = 'Closed with: ${result}';
+    }, (reason) => {
+      this.closeResult = 'Dismissed ${this.getDismissReason(reason)}';
+    });
+  }
+  following(content) {
+
+    this.modalService.open(content, { size: 'sm' }).result.then((result) => {
+      this.closeResult = 'Closed with: ${result}';
+    }, (reason) => {
+      this.closeResult = 'Dismissed ${this.getDismissReason(reason)}';
+    });
+  }
+  getWhoFollow() {
+    if (this.CurrentLoggedUserId) {
+
+      this.viewService.getView('who-follow', [['uid', this.CurrentLoggedUserId], ['page', this.pages]]).subscribe(data => {
+        if (data[0]) {
+          //this.countLikers = data[0]['likes_count'];
+        }
+        this.whoFollow = this.whoFollow.concat(data);
+       // this.whoFollow = data;
+        console.log(data)
+        this.loadMoreVisibilty();
+      });
+    }
+  }
+  getWhoFollowing() {
+    if (this.CurrentLoggedUserId) {
+      this.viewService.getView('who-following', [['uid', this.CurrentLoggedUserId]]).subscribe(data => {
+        if (data[0]) {
+          //this.countLikers = data[0]['likes_count'];
+        }
+        //this.whoFollowing = this.whoFollowing.concat(data);
+        this.whoFollowing = data;
+        console.log(data)
+        //this.loadMoreVisibilty();
+      });
+    }
+  }
+  /* function load more  */
+  loadMoreFollow() {
+    this.pages++;
+    this.getWhoFollow();
+  }
+  /* end function load more  */
+  // Function to control load more button
+  loadMoreVisibilty() {
+    // get the challenges array count
+    if (this.countFollowers <= this.whoFollow.length) {
+
+      this.hideloadmorefollow = true;
+
+    } else if (this.countFollowers > this.whoFollow.length) {
+      this.hideloadmorefollow = false;
+    }
+  }
+  /* END FUNCTION loadMoreVisibilty */
+   /* function load more  */
+  loadMoreFollowing() {
+    this.pages++;
+    this.getWhoFollow();
+  }
+  /* end function load more  */
+  // Function to control load more button
+  loadMoreVisibiltyFollowing() {
+    // get the challenges array count
+    if (this.countFollowing <= this.whoFollowing.length) {
+
+      this.hideloadmorefollowing = true;
+
+    } else if (this.countFollowing > this.whoFollowing.length) {
+      this.hideloadmorefollowing = false;
+    }
+  }
+  /* END FUNCTION loadMoreVisibilty */
 
 }
