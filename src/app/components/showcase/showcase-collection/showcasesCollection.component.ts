deabb5cf95a702bcd0ad05b0552681404c844468
A.Abdelraheem <a.abdelraheem@orangestudio.com>
2017-07-17 11:27:46 +0200

    Showcases Collection Component edited and showcaseSorting component removed

--- src/app/components/showcase/showcase-collection/showcasesCollection.component.ts
+++ src/app/components/showcase/showcase-collection/showcasesCollection.component.ts
@@ -1,24 +1,24 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 import { ViewService } from './../../../d7services';
-import { ISorting } from '../../../models/challenge/sorting';
+import { SortBySortingSet, SortingSet } from '../../../models/makers/sorting';
 import { LoaderService } from '../../shared/loader/loader.service';
 
 @Component({
   selector: 'app-showcases',
   templateUrl: './showcasesCollection.component.html',
 })
+
 export class ShowcasesCollectionComponent implements OnInit {
   showcases = [];
-  showcaseCount = 0;
-  hideloadmore = false;
-  loadFlag = false;
-  sortData: ISorting;
-  sort_order: string;
-  sort_by: string;
-  limit = 4;
-
-  @Input() sortType: ISorting;
+  showcaseCount: number = 0;
+  hideloadmore: boolean = false;
+  pageNo: number = 0;
+  CurrentSort: SortingSet = {
+    sort_by: 'changed',
+    sort_order: 'DESC'
+  }
+  SortBy: SortBySortingSet = new SortBySortingSet(this.CurrentSort, this.viewService);
 
   constructor(
     private viewService: ViewService,
@@ -27,73 +27,58 @@ export class ShowcasesCollectionComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.sort_order = "DESC";
-    this.sort_by = "changed";
     this.showcasesCount();
     this.getShowCases();
-
-  }
-
-  ShowSingleShowcase(nid) {
-    this.router.navigate(['/makers', nid]);
   }
 
   getShowCases() {
     // show spinner
     this.loaderService.display(true);
-
-    // load the showcases
-    if (this.loadFlag) {
-      this.limit += 3;
+    if (this.pageNo == 0) {
+      this.showcases = [];
     }
-    this.viewService.getView('views/showcases', [['display_id', 'services_1'], ['limit', this.limit], ['sort_by', this.sort_by], ['sort_order', this.sort_order]]).subscribe(data => {
-      this.showcases = data;
+    // load the showcases
+    this.SortBy.Sort('showcases', this.pageNo).subscribe(data => {
+      this.showcases = this.showcases.concat(data);
       this.loadMoreVisibilty();
       // hide spinner
       this.loaderService.display(false);
 
     }, err => {
       // hide spinner
-      this.loaderService.display(false);      
+      this.loaderService.display(false);
     });
-    this.loadFlag = false;
   }
-
   // get more click
   loadmore() {
-    this.loadFlag = true;
+    this.pageNo++;
     this.getShowCases();
   }
   // control load more button
   loadMoreVisibilty() {
     // get the challenges array count
     if (this.showcases.length >= this.showcaseCount) {
-      //  console.log("flage");
       this.hideloadmore = true;
-      // console.log(this.hideloadmore);
     } else {
       this.hideloadmore = false;
     }
-
   }
-
-
-  getSortType(event: any) {
-    this.sortData = event;
-    this.sort_by = this.sortData.sort_by;
-    this.sort_order = this.sortData.sort_order;
+  SortShowcases(sort) {
+    if (sort == '_none') {
+      return;
+    }
+    this.CurrentSort.sort_order = 'DESC';
+    if (sort == 'changed_1' || sort == 'title') {
+      this.CurrentSort.sort_order = 'ASC';
+    }
+    this.CurrentSort.sort_by = sort;
+    this.pageNo = 0;
     this.getShowCases();
-    //console.log(this.getProjects);
   }
-
-
   // get the count of showcases
   showcasesCount() {
     this.viewService.getView('maker_count_showcases', []).subscribe(data => {
       this.showcaseCount = data[0];
-      // console.log(this.showcaseCount);
     });
   }
-
-
 }
