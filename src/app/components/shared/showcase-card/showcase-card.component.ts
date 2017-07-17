098131f2dff9505dc9c0dccb0aac5f5a9d21a039
A.Abdelraheem <a.abdelraheem@orangestudio.com>
2017-07-17 14:02:51 +0200

    showcase card in progress

--- src/app/components/shared/showcase-card/showcase-card.component.ts
+++ src/app/components/shared/showcase-card/showcase-card.component.ts
@@ -8,82 +8,20 @@ import { ShowcaseCard } from '../../../models/cards/showcaseCard'
   templateUrl: './showcase-card.component.html',
 })
 export class ShowcaseCardComponent implements OnInit {
-  // showcase;
   userId;
-  // makersCount;
-  // projectsCount;
-  // numLikes;
-  // Makers = [];
-  // Projects = [];
-  // contentType: number = 2;
-  // showcaseType: string = 'Makers';
-  // pageNumber = 0;
-  // Manager: boolean = false;
   @Input() showcaseCard:ShowcaseCard;
+  test:ShowcaseCard;
   constructor(private route: ActivatedRoute,
     private router: Router,
     // private viewService: ViewService,
     // private flagService: FlagService,
     // public auth: Auth,
-
-
   ) { }
   ngOnInit() {
-    console.log(this.showcaseCard)
-    // this.auth.IsCommuintyManager();
-    // this.Manager = this.auth.IsCommuintyManager();
-    // this.getShowcase();
-    // this.getShowcases();
+    console.log(this.test);
     this.userId = localStorage.getItem('user_id');
-    // this.countLikes();
-    // this.getMakersCount();
-    // this.getProjectsCount();
-  }
-
-  getShowcase() {
-    // this.viewService.getView('shared-showcase-card', [['nid', this.showcaseNid]]).subscribe(data => {
-    //   this.showcase = data[0];
-    // });
-  }
-  getShowcases() {
-    // this.viewService.getView('showcase', [['nid', this.showcaseNid]])
-    //   .subscribe(data => {
-    //     this.contentType = data[0]['showcase_type'];
-    //     if (this.contentType == 1) {
-    //       this.getProjectsCount();
-    //       this.showcaseType = 'Projects';
-    //     } else if (this.contentType == 2) {
-    //       this.getMakersCount();
-    //       this.showcaseType = 'Makers';
-    //     }
-    //   });
-  }
-  getMakersCount() {
-    // this.viewService.getView('maker_count_showcases/' + this.showcaseNid).subscribe(data => {
-    //   this.makersCount = data;
-    // });
-
-  }
-  getProjectsCount() {
-    // this.viewService.getView('showcase_project_count/', [['nid', this.showcaseNid]]).subscribe(data => {
-
-    //   this.projectsCount = data[0].project_count;
-    // });
-
   }
   ShowSingleShowcase(path) {
     this.router.navigate(['showcases', path]);
   }
-
-  countLikes() {
-  //   this.flagService.flagCount(this.showcaseNid, 'like').subscribe(res => {
-  //     if (res['count'] > 0) {
-  //       this.numLikes = res;
-  //     } else {
-  //       this.numLikes = 0;
-  //     }
-  //   })
-  }
-
-
 }
