3f0b130eb3f864ca9416524095306f96b140f645
youssef <y.salah@orangestudio.com>
2017-07-24 10:21:26 +0200

    orgs component

---
+++ src/app/components/orgs/orgs.routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { OrgsComponent } from './orgs.component';
+
+
+const orgsRouts: Routes = [
+   { path: '', component:OrgsComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(orgsRouts),
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+})
+
+
+export class OrgsRoutingModule { }
