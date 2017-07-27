bbd856367a38a4021657cb95f2793c0b0dc17e88
youssef <y.salah@orangestudio.com>
2017-07-27 13:43:50 +0200

    add out team orgs-component

--- src/app/components/orgs/orgs.module.ts
+++ src/app/components/orgs/orgs.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { OrgsComponent } from './orgs.component';
 import { OrgsRoutingModule } from './orgs.routing.module';
 import { OrgsFollowersComponent } from '../orgs/orgs-followers/orgs-followers.component';
-
 import { OrgsAboutUsComponent } from './orgs-about-us/orgs-about-us.component';
 import { OrgsProjectsComponent } from './orgs-projects/orgs-projects.component';
 import { SharedModule } from '../shared/shared.module';
