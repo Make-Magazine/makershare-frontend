12bda4f7561d21b63d29082107a92eb49fcef8da
youssef <y.salah@orangestudio.com>
2017-08-01 15:46:52 +0200

    contact company

---
+++ src/app/d7services/globals.ts
@@ -0,0 +1,7 @@
+'use strict';
+export var appURL = 'http://localhost:4200';
+export var domain = 'http://localhost:333';
+// export var domain = 'https://preview-manage.makershare.com';
+export var solrPath = 'http://192.168.0.208:333/solr/drupal/';
+export var endpoint = '/api';
+export var appName = 'Maker Share';
