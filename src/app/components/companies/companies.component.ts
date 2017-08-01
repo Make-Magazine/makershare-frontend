ae9651ac70f4a9b72f4137e21ded6a02f288b8b9
a.fawzi <a.fawzi@orangestudio.com>
2017-08-01 13:16:24 +0200

    refactoring companies filter and sort loadmore

--- src/app/components/companies/companies.component.ts
+++ src/app/components/companies/companies.component.ts
@@ -3,7 +3,6 @@ import { ViewService, MainService } from '../../d7services';
 import { SortBySortingSet, SortingSet } from '../../models';
 import * as globals from '../../d7services/globals';
 import { LoaderService } from '../shared/loader/loader.service';
-import { NotificationBarService, NotificationType } from 'ngx-notification-bar/release';
 
 @Component({
   selector: 'app-companies',
@@ -17,85 +16,55 @@ export class CompaniesComponent implements OnInit {
     sort_order: "DESC",
   };
   SortBy: SortBySortingSet = new SortBySortingSet(this.CurrentSortSet, this.viewService);
-  companies;
+  companies = [];
   comapnyFilter = "All";
   pages: number = 0;
   countCompanies = 0;
   showloadmoreCompany = false;
-
   constructor(
     private loaderService: LoaderService,
-    private notificationBarService: NotificationBarService,
     private viewService: ViewService,
     private mainService: MainService) { }
-
   ngOnInit() {
     this.getCountCompanies();
-    this.getCompanies();
+    this.getCompanies(false);
   }
-  getCompanies() {
+  getCompanies(more?: boolean) {
     this.loaderService.display(true);
-    if (this.pages == 0) {
-      this.companies = [];
-    }
+    if (more) this.pages++;
     this.SortBy.Sort('company_cards', this.pages, null, this.comapnyFilter).subscribe(data => {
       this.companies = this.companies.concat(data);
-      console.log(this.companies);
-      this.loadMoreVisibilty();
-      if (this.companies.length == 0) {
-        this.notificationBarService.create({ message: "There aren't any projects yet for this topic. Go make one!", type: NotificationType.Error, allowClose: false, autoHide: true, hideOnHover: false });
-      }
+      this.showloadmoreCompany = (this.countCompanies <= this.companies.length) ? false : true;
       this.loaderService.display(false);
     });
   }
-
   selectCompanyType(value) {
-    // console.log(value);
-    let body = {
-      "value": value,
-    };
-    this.mainService.post(globals.endpoint + '/company_profile_api/retrieve_count_of_companies_startup_or_non_profit', body).map(res => res.json()).subscribe(res => {
-      this.countCompanies = res;
-    }, err => {
-      // this.notificationBarService.create({ message: "Sorry, but your project doesn't meet the challenge requirements, Please check <a id='rules-id' href='#rules' data-nodeId='" + this.nid + "'>Rules & Instructions </a>", type: NotificationType.Error, allowClose: true, autoHide: false, hideOnHover: false, isHtml: true });
-    });
-    if (value == 'All') {
-      this.getCountCompanies();
+    this.companies = [];
+    this.pages = 0;
+    let body = { "value": value };
+    if (value == 'All') { this.getCountCompanies() } else {
+      this.mainService.post(globals.endpoint + '/company_profile_api/retrieve_count_of_companies_startup_or_non_profit', body).map(res => res.json()).subscribe(res => {
+        this.countCompanies = res;
+      });
     }
     this.comapnyFilter = value;
-    this.getCompanies();
-
+    this.getCompanies(false);
   }
   /* function to get count companies */
   getCountCompanies() {
     this.mainService.post(globals.endpoint + '/company_profile_api/retrieve_count_of_companies').map(res => res.json()).subscribe(res => {
       this.countCompanies = res[0];
-     // console.log(this.countCompanies);
-    }, err => {
-      // this.notificationBarService.create({ message: "Sorry, but your project doesn't meet the challenge requirements, Please check <a id='rules-id' href='#rules' data-nodeId='" + this.nid + "'>Rules & Instructions </a>", type: NotificationType.Error, allowClose: true, autoHide: false, hideOnHover: false, isHtml: true });
     });
   }
-
   sortCompanies(sort) {
+    this.companies = [];
+    this.pages=0;
     if (sort == '_none') return;
-    //this.pages = 0;
     this.CurrentSortSet.sort_order = "DESC";
     if (sort == 'created_1' || sort == 'title') {
       this.CurrentSortSet.sort_order = "ASC";
     }
     this.CurrentSortSet.sort_by = sort;
-    //this.getCountProject();
-    this.getCompanies();
-  }
-  loadMoreCompany() {
-    this.pages++;
-    this.getCompanies();
-  }
-  loadMoreVisibilty() {
-    if (this.countCompanies <= this.companies.length) {
-      this.showloadmoreCompany = false;
-    } else if (this.countCompanies > this.companies.length) {
-      this.showloadmoreCompany = true;
-    }
+    this.getCompanies(false);
   }
 }
