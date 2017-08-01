fbdb31bd238773badad50a8c8174deeaaba8c731
youssef <y.salah@orangestudio.com>
2017-08-01 16:24:57 +0200

    fix error count_projects_in_orgs

--- src/app/Modules/shared/maker-card/maker-card.component.ts
+++ src/app/Modules/shared/maker-card/maker-card.component.ts
@@ -51,7 +51,7 @@ export class MakerCardComponent implements OnInit {
   getMakerCard() {
     this.viewService.getView('maker_card_data', [['uid', this.uid]]).subscribe(data => {
       this.card = data[0];
-      console.log(this.card)
+      console.log(this.card.org_logo)
     });
   }
 
