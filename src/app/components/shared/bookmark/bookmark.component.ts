338cc4edf1a10fd6a3ad965cc6b1b4c99d0383b1
Matthieu COLLÉ <druidev+github@gmail.com>
2017-08-01 09:02:35 -0500

    (refactor) cleaning & simplification of social components

--- src/app/components/shared/bookmark/bookmark.component.ts
+++ src/app/components/shared/bookmark/bookmark.component.ts
@@ -1,59 +1,56 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FlagService,UserService } from '../../../d7services';
-// import { NotificationBarService, NotificationType } from 'ngx-notification-bar/release';
 import { NgbTooltipConfig } from '@ng-bootstrap/ng-bootstrap';
+import { FlagService, UserService } from '../../../d7services';
 
 @Component({
   selector: 'app-bookmark',
   templateUrl: './bookmark.component.html',
   providers: [NgbTooltipConfig],
-
 })
 export class BookmarkComponent implements OnInit {
+  @Input() nodeNid;
+  @Input() nodeType: string = '';
+  userId;
+  private checkUserLogin: boolean = false;
+  private isBookmarked: boolean;
+  private toggleFlag: string;
 
   constructor(
     private router: Router,
     private userService: UserService,
     private flagService: FlagService,
-    // private notificationBarService: NotificationBarService,
     private config: NgbTooltipConfig,
   ) {
     this.router = router;
     this.config.placement = 'bottom';
     this.config.triggers = 'hover';
   }
-  @Input() nodeNid;
-  @Input() nodeType: string = '';
-  userId;
-  checkUserLogin = false;
 
-  isBookmarked;
-  bookmark: string;
-  toggleFlag:string;
   ngOnInit() {
     this.userId = localStorage.getItem('user_id');
     this.userService.isLogedIn().subscribe(data => {
       this.checkUserLogin = data;
       if (this.nodeNid) {
-        this.flagService.isFlagged(this.nodeNid, this.userId, 'node_bookmark').subscribe(data => {
-          this.isBookmarked = data[0];
-          this.bookmark = (this.isBookmarked? 'Unbookmark this ':'Bookmark this ') + this.nodeType;
-        })
+        this.flagService
+          .isFlagged(this.nodeNid, this.userId, 'node_bookmark')
+          .subscribe(data => {
+            this.isBookmarked = data[0];
+          });
       }
     });
   }
 
   bookmarkThis(e: Event) {
-      if (!this.checkUserLogin) {
-        this.router.navigate(['/access-denied']);
-      } else {
-        this.toggleFlag = this.isBookmarked? 'unflag':'flag';
-        this.flagService[this.toggleFlag](this.nodeNid, this.userId, 'node_bookmark').subscribe(response => {
-            this.isBookmarked = !this.isBookmarked;
-            this.bookmark = (this.isBookmarked? 'Unbookmark this ':'Bookmark this ') + this.nodeType;
-        });         
-      }
+    if (!this.checkUserLogin) {
+      this.router.navigate(['/access-denied']);
+    } else {
+      this.toggleFlag = this.isBookmarked ? 'unflag' : 'flag';
+      this.flagService
+        [this.toggleFlag](this.nodeNid, this.userId, 'node_bookmark')
+        .subscribe(response => {
+          this.isBookmarked = !this.isBookmarked;
+        });
+    }
   }
-
 }
---
+++ src/app/components/shared/likes/likes.component.ts
@@ -0,0 +1,59 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NgbTooltipConfig } from '@ng-bootstrap/ng-bootstrap';
+import { FlagService, UserService } from '../../../d7services';
+
+@Component({
+  selector: 'app-likes',
+  templateUrl: './likes.component.html',
+  providers: [NgbTooltipConfig],
+})
+export class LikesComponent implements OnInit {
+  @Input() nodeNid;
+  @Input() likesCount: number;
+  @Output() countNumber = new EventEmitter<number>();
+  @Input() userId;
+  countlikes: number = 0;
+  toggleFlag: string;
+  isLiked = false;
+
+  constructor(
+    private userService: UserService,
+    private flagService: FlagService,
+    private config: NgbTooltipConfig,
+  ) {
+    this.config.placement = 'bottom';
+    this.config.triggers = 'hover';
+  }
+
+  ngOnInit() {
+    this.countLikes();
+    this.userId = localStorage.getItem('user_id');
+    this.userService.isLogedIn().subscribe(data => {
+      if (data && this.userId) {
+        this.flagService
+          .isFlagged(this.nodeNid, this.userId, 'like')
+          .subscribe(data => {
+            this.isLiked = data[0];
+          });
+      }
+    });
+  }
+
+  countLikes() {
+    this.flagService.flagCount(this.nodeNid, 'like').subscribe(response => {
+      this.countlikes = response['count'] ? response['count'] : 0;
+      this.countNumber.emit(this.countlikes);
+    });
+  }
+
+  likeThis(e: Event) {
+    e.preventDefault();
+    this.toggleFlag = this.isLiked ? 'unflag' : 'flag';
+    this.flagService
+      [this.toggleFlag](this.nodeNid, this.userId, 'like')
+      .subscribe(response => {
+        this.countlikes = this.isLiked ? --this.countlikes : ++this.countlikes;
+        this.isLiked = !this.isLiked;
+      });
+  }
+}
