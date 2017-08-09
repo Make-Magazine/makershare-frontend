ecc4941a312b8997e0015f673c35b5926c1144a0
Matthieu COLLÉ <druidev+github@gmail.com>
2017-08-09 15:32:32 -0500

    (feat) started profile page

--- src/app/core/models/application/settings.ts
+++ src/app/core/models/application/settings.ts
@@ -1,16 +1,15 @@
 export class Settings {
-
   apiEndPoint: string = 'api';
 
   // // connect to stage
-  // apiProtocol: 'http'|'https' = 'https';
-  // apiHost: string = 'preview-manage.makershare.com';
-  // apiPort: number;
+  apiProtocol: 'http' | 'https' = 'https';
+  apiHost: string = 'preview-manage.makershare.com';
+  apiPort: number;
 
   // connect locally
-  apiProtocol: 'http'|'https' = 'http';
-  apiHost: string = 'localhost';
-  apiPort: number = 333;
+  // apiProtocol: 'http'|'https' = 'http';
+  // apiHost: string = 'localhost';
+  // apiPort: number = 333;
 
   solrPath: string = 'https://preview-manage.makershare.com/solr/drupal/';
   appURL: string = 'http://localhost:4200';
@@ -25,5 +24,4 @@ export class Settings {
   getBackEndUrlWithEndPoint(): string {
     return this.getBackEndUrl() + this.apiEndPoint + '/';
   }
-
 }
