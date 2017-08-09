af069378ba37e07181a71bf6907ca248a42e7e06
yaser.marey <w.nabil@orangestudio.com>
2017-08-09 13:53:53 +0200

    apply some design patterns to drupal nodes

--- src/app/core/models/application/settings.ts
+++ src/app/core/models/application/settings.ts
@@ -3,16 +3,16 @@ export class Settings {
   apiEndPoint: string = 'api';
 
   // // connect to stage
-  apiProtocol: 'http'|'https' = 'https';
-  apiHost: string = 'preview-manage.makershare.com';
-  apiPort: number;
+  // apiProtocol: 'http'|'https' = 'https';
+  // apiHost: string = 'preview-manage.makershare.com';
+  // apiPort: number;
 
   // connect locally
-  // apiProtocol: 'http'|'https' = 'http';
-  // apiHost: string = 'localhost';
-  // apiPort: number = 333;
+  apiProtocol: 'http'|'https' = 'http';
+  apiHost: string = 'localhost';
+  apiPort: number = 333;
 
-  solrPath: string = 'http://192.168.0.208:333/solr/drupal/';
+  solrPath: string = 'https://preview-manage.makershare.com/solr/drupal/';
   appURL: string = 'http://localhost:4200';
   appName: string = 'Maker Share';
   language: string = 'und';
