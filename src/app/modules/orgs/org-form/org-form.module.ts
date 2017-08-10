5f00056eb9bcecc42e6692f48fe09108d2e32f9d
yaser.marey <w.nabil@orangestudio.com>
2017-08-10 11:35:00 +0200

    adding form group to html and import required form modules

--- src/app/modules/orgs/org-form/org-form.module.ts
+++ src/app/modules/orgs/org-form/org-form.module.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../../shared/shared.module';
 import { AboutUsComponent } from '../../orgs/org-form/about-us/about-us.component';
 import { BasicInfoComponent } from '../../orgs/org-form/basic-info/basic-info.component';
 import { OrgsRoutingModule } from '../../orgs/org-form/org-form.routing.module';
@@ -7,7 +9,7 @@ import { OrgFormComponent } from '../../orgs/org-form/org-form.component';
 import { ProjectsComponent } from '../../orgs/org-form/projects/projects.component';
 
 @NgModule({
-  imports: [CommonModule, OrgsRoutingModule],
+  imports: [CommonModule, OrgsRoutingModule, SharedModule.forChild(), ReactiveFormsModule],
   declarations: [
     OrgFormComponent,
     BasicInfoComponent,
