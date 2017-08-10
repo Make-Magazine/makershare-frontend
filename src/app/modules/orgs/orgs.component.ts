4604cccd76734b7298ad39c6e8ecdfdd33b80b3a
a.fawzi <a.fawzi@orangestudio.com>
2017-08-10 16:17:45 +0200

    add follow orgs component

--- src/app/modules/orgs/orgs.component.ts
+++ src/app/modules/orgs/orgs.component.ts
@@ -42,6 +42,7 @@ export class OrgsComponent implements OnInit {
         if (this.nid) {
           this.viewService.getView('company_profile_api/' + this.nid).subscribe(data => {
             this.company = data;
+            console.log(this.company)
             if (this.company) {
               this.team = this.company['field_maker_memberships'];
             }
@@ -67,6 +68,7 @@ export class OrgsComponent implements OnInit {
     };
     this.mainService.custompost('company_profile_api/retrieve_count_of_company_followers', body).subscribe(res => {
       this.followers = res['followers'];
+      console.log(this.followers)
       this.followersCount = res['count_all'];
     });
   }
@@ -74,8 +76,13 @@ export class OrgsComponent implements OnInit {
     if (more) this.page++;
     this.viewService.getView('company_followers', [['page', this.page], ['nid', this.nid]]).subscribe(data => {
       this.allFollwers = this.allFollwers.concat(data);
+      console.log(this.allFollwers)
       this.showloadmoreFollowers = (this.followersCount <= this.allFollwers.length) ? false : true;
     });
+  }
+  followersCounter(count) {
+    this.getLimitedFollowers();
+    
   }
   open(content) {
     this.modalService.open(content, { size: 'sm' }).result.then((result) => {
