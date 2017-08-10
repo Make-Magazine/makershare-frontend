bcc84d7b7e402bc0c5019513294d4fd6a6b9c6c2
yaser.marey <w.nabil@orangestudio.com>
2017-08-10 11:18:23 +0200

    build org form

--- src/app/modules/orgs/org-form/org-form.component.ts
+++ src/app/modules/orgs/org-form/org-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Organization, EntityProxy } from '../../../core/models';
 import { FileService, NodeService } from '../../../core/d7services';
 
@@ -11,8 +12,14 @@ export class OrgFormComponent implements OnInit {
   // Current active form tab, the default is Basic Info
   currentFormTab:string = 'Basic Info';
   organizationProxy = new EntityProxy(new Organization());
+
+  organizationForm:FormGroup;
   
-  constructor(private nodeService: NodeService, private fileService: FileService) {}
+  constructor(
+    private nodeService: NodeService,
+    private fileService: FileService,
+    private formBuilder: FormBuilder,
+  ) {}
 
   ngOnInit() {
     this.fileService;
@@ -23,13 +30,36 @@ export class OrgFormComponent implements OnInit {
     this.organizationProxy.field_orgs_cover_photo.fid = 3618;
     this.organizationProxy.field_orgs_contact.email = 'example@bla.com';
     this.organizationProxy.field_minimum_number_of_follower.value = 4987;
+    this.buildForm();
   }
 
   // Fires when clicking on publish button
-  publishButtonClick(){
+  publishButtonClick() {
     console.log(this.organizationProxy);
     this.nodeService.createNode(this.organizationProxy.entity).subscribe(data => {
       console.log(data);
+    });
+  }
+
+  buildForm() {
+    this.organizationForm = this.formBuilder.group({
+      title: [this.organizationProxy.title, [Validators.required]],
+      field_orgs_type: [this.organizationProxy.field_orgs_type, [Validators.required]],
+      field_orgs_logo: [this.organizationProxy.field_orgs_logo, [Validators.required]],
+      field_orgs_cover_photo: [this.organizationProxy.field_orgs_cover_photo, [Validators.required]],
+      field_orgs_contact: [this.organizationProxy.field_orgs_contact.email, [Validators.required, Validators.email]],
+      field_orgs_phone: [this.organizationProxy.field_orgs_phone.value, []],
+      field_founder_name: [this.organizationProxy.field_founder_name.value, []],
+      field_founded_date: [this.organizationProxy.field_founded_date, []],
+      field_type_of_business: [this.organizationProxy.field_type_of_business.value, []],
+      field_social_accounts: [this.formBuilder.array([])],
+      field_website_blog: [this.organizationProxy.field_website_blog.value, []],
+      field_orgs_address: [this.organizationProxy.field_orgs_address, []],
+      field_minimum_number_of_follower: [this.organizationProxy.field_minimum_number_of_follower.value, []],
+      field_breif_info: [this.organizationProxy.field_breif_info.value, []],
+      body: [this.organizationProxy.body.value, []],
+      field_maker_memberships: this.formBuilder.array([]),
+      field_orgs_projects: this.formBuilder.array([]),
     });
   }
 }
