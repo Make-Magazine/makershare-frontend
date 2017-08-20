8e9f610dac6e8e32e23f56791a17fef149a245f5
Jared Smith <jared@makermedia.com>
2017-08-20 10:03:43 -0700

    updates to login flow

--- src/app/modules/auth0/login/login.component.ts
+++ src/app/modules/auth0/login/login.component.ts
@@ -9,14 +9,42 @@ import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class LoginComponent implements OnInit {
   private CancelTitle: string = 'Cancel';
   private closeResult: string;
+  resetSent;
+  selected_day = '';
+  selected_month = '';
+  selected_year = '';
+  submitted = false;
+  forgetEmail = {
+    email: ''
+  };
 
-  current_active_tab: string = 'login';
-
+  userlogin = {
+    email: '',
+    password: ''
+  };
+  userSignup = {
+    emailUp: '',
+    passwordUp: '',
+    firstName: '',
+    lastName: '',
+    birthdate: 0,
+    checkbox: 0,
+    month: '',
+    day: '',
+    year: []
+  }
   constructor(
     public auth: Auth,
-    private modalService: NgbModal) { }
+    private modalService: NgbModal
+  ) { }
 
   ngOnInit() {
+          var index=0;
+
+    for (let i = 1930; i < 2017; i++) {
+        this.userSignup.year[index] = i;
+        index++;
+  }
   }
   open(content) {
     this.modalService.open(content).result.then(
@@ -39,5 +67,26 @@ export class LoginComponent implements OnInit {
       return `with: ${reason}`;
     }
   }
+  public  (email, password) {
 
+    if (email && password) {
+      this.auth.login(email, password);
+    }
+
+  }
+
+  public resetPassword(email) {
+    this.auth.resetPassword(email);
+    this.resetSent = true;
+  }
+
+  public signup(user) {
+
+    this.submitted = true;
+    if (user.emailUp && user.passwordUp && user.firstName && user.lastName && user.checkbox && this.selected_month && this.selected_day && this.selected_year) {
+      this.auth.signup(user.emailUp, user.passwordUp, user.firstName, user.lastName, user.month, user.day, this.selected_year, user.birthdate, user.checkbox)
+    }
+  }
+
+ 
 }
