caedc9f098a4761fe0684d5f7b24eda4d4a5952d
Abdulrahman Alhomsi <a.alhomsi@orangestudio.com>
2017-08-28 11:42:29 +0200

    build

---
+++ src/app/core/models/application/settings.ts
@@ -0,0 +1,35 @@
+export class Settings {
+    
+      apiEndPoint: string = 'api';
+    
+      // // connect to stage
+      // apiProtocol: 'http'|'https' = 'https';
+      // apiHost: string = 'preview-manage.makershare.com';
+      // apiPort: number;
+    
+      // connect locally
+      apiProtocol: 'http'|'https' = 'http';
+      apiHost: string = 'localhost';
+      appURL: string = 'https://preview.makershare.com';
+      apiPort: number = 333;
+    
+      solrPath: string = 'http://192.168.0.208:333/solr/drupal/';
+      // appURL: string = 'http://localhost:4200';
+      appName: string = 'Maker Share';
+      language: string = 'und';
+    
+      orgCreated: boolean = false;
+      orgDeleted: boolean = false;
+    
+    
+      getBackEndUrl(): string {
+        const url = this.apiProtocol + '://' + this.apiHost;
+        return this.apiPort ? url + ':' + this.apiPort + '/' : url + '/';
+      }
+    
+      getBackEndUrlWithEndPoint(): string {
+        return this.getBackEndUrl() + this.apiEndPoint + '/';
+      }
+    
+    }
+    
