3463ac05be7adfd8acb654391a28ac2c8c5e371c
Jared Smith <jared@makermedia.com>
2017-09-01 11:34:41 -0700

    adds pmx build script

---
+++ remoteBuild.js
@@ -0,0 +1,15 @@
+var pmx = require('pmx').init({
+  http          : true, // HTTP routes logging (default: true)
+  ignore_routes : [/socket\.io/, /notFound/], // Ignore http routes with this pattern (Default: [])
+  errors        : true, // Exceptions logging (default: true)
+  custom_probes : true, // Auto expose JS Loop Latency and HTTP req/s as custom metrics
+  network       : true, // Network monitoring at the application level
+  ports         : true  // Shows which ports your app is listening on (default: false)
+});
+
+pmx.action('build', function(reply) {
+  build(function() {
+    exec('npm run buildall');
+	reply({success : true});
+  });
+});
