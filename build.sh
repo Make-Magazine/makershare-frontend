a70346b6ef18e00357edcee1d98002df00b96f71
Jared Smith <jared@makermedia.com>
2017-09-01 10:10:53 -0700

    removes sudo from build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 npm install
 npm run build
 pm2 stop all 
-sudo cp -r src/assets dist/assets
+cp -r src/assets dist/assets
 sass dist/assets/css/style.scss dist/assets/css/style.css
 pm2 restart all
