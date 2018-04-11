5e2484487b79e01dcf1408b65bef9b9b92727f92
Rio Roth-Barrerio <rio@makermedia.com>
2018-04-11 15:03:26 -0700

    Add back in some of that angular logic

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -407,7 +407,6 @@ $(window).on('load', function () {
 //////////////////////////////////////
 
 $(window).on('load', function () {   
-  console.log("putty");
   $('#hamburger-icon, #hamburger-makey, .nav-flyout-underlay').click(function() {
     $('#hamburger-icon').toggleClass('open');
     $('#hamburger-makey').animate({opacity: 'toggle'});
