22b5f01de211a4c670034e5a12955a3c3f6bc3c9
Rio Roth-Barrerio <rio@makermedia.com>
2018-04-10 17:09:02 -0700

    Get that javascript added and refine styling

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -402,3 +402,149 @@ $(window).on('load', function () {
   document.getElementById('homeBanner').style.backgroundImage = 'url(../assets/images/home-hero-images/' + images[Math.floor(Math.random() * images.length)] + ')';
 });
 
+//////////////////////////////////////
+/////// Navigation and Search ////////
+//////////////////////////////////////
+
+(function($) {   
+  $('#hamburger-icon, #hamburger-makey, .nav-flyout-underlay').click(function() {
+    $('#hamburger-icon').toggleClass('open');
+    $('#hamburger-makey').animate({opacity: 'toggle'});
+    $('#nav-flyout').animate({opacity: 'toggle'});
+    $('body').toggleClass('nav-open-no-scroll');
+    $('.nav-flyout-underlay').animate({opacity: 'toggle'});
+  });
+
+  $('.nav-flyout-column').on('click', '.expanding-underline', function(event) {
+    if ($(window).width() < 577) { 
+      event.preventDefault();
+      $(this).toggleClass('underline-open');
+      $(this).next('.nav-flyout-ul').slideToggle();
+    }
+  });
+  // fix nav to top on scrolldown, stay fixed for transition from mobile to desktop
+  var e = $(".universal-nav");
+  var hamburger = $(".nav-hamburger");
+  var y_pos = $(".nav-level-2").offset().top;
+  var nextItemUnderNav = $("#home-featured");
+    if($(".second-nav").length) {
+        nextItemUnderNav = $(".second-nav");
+    }
+  $(window).on('resize', function(){
+      if ($(window).width() < 767) {
+          y_pos = 0;
+          nextItemUnderNav.css("margin-top", "55px");
+      }else{
+          y_pos = 75;
+          nextItemUnderNav.css("margin-top", "0px");
+      }
+  });
+  jQuery(document).scroll(function() {
+      var scrollTop = $(this).scrollTop();
+      if(scrollTop > y_pos && $(window).width() > 767){
+          e.addClass("main-nav-scrolled"); 
+          hamburger.addClass("ham-menu-animate");
+          nextItemUnderNav.css("margin-top", "55px");
+      }else if(scrollTop <= y_pos){
+          e.removeClass("main-nav-scrolled"); 
+          hamburger.removeClass("ham-menu-animate");
+          if ($(window).width() > 767) {
+            nextItemUnderNav.css("margin-top", "0px");
+          }
+      }
+  });
+    
+  $("#search-modal").fancybox({
+        wrapCSS : 'search-modal-wrapper',
+        autoSize : true,
+        //width  : 400,
+        autoHeight : true,
+        padding : 0,
+        overlay: {
+            opacity: 0.8, // or the opacity you want 
+            css: {'background-color': '#ff0000'} // or your preferred hex color value
+        }, // overlay 
+        closeClick  : false, 
+        afterShow   : function() {
+            // keep it from reloading upon clicks
+            $('#search-modal').bind('click', false);
+            $(".sb-search-submit").click(function(e){
+                if($("#search").val() && $("#search").val() != ""){
+                    var searchForm = $(".search-form");
+                    window.location.href = searchForm.attr("action") +"?s=" + $("#search").val();
+                }else{
+                    $("#search").attr('placeholder', "Please enter in some text to search for...");            
+                }
+            });
+            $(".sb-search-input").focus();
+        },
+        afterClose: function () {
+            $('#search-modal').unbind('click', false);
+        }
+  });
+
+  $(".fa-search").click(function(e){
+       $("#search-modal").trigger('click');
+       // essb sharing popup is being triggered when logged in to admin
+       $(".essb-live-customizer-main, .essb-live-buttons-customizer").attr('style', 'display: none !important');
+  });
+    
+  // to keep this nav universal, let's not have each site's style sheet highlight a different active manually
+  var site = window.location.hostname;
+  var firstpath = $(location).attr('pathname');
+    firstpath.indexOf(1);
+    firstpath.toLowerCase();
+    firstpath = firstpath.split("/")[1];
+  var shareSection = site + "/" + firstpath;
+  function universalNavActive( site ) {
+    jQuery(".nav-" + site).addClass("active-site");
+    jQuery(".nav-" + site + " .nav-level-2-arrow").addClass("active-site")
+  }
+  // each one has to apply to a number of environments
+  switch(site) {
+    case "make-zine":
+    case "makezine":
+    case "makezine.wpengine.com":
+    case "makezine.staging.wpengine.com":
+    case "makezine.com":
+        universalNavActive("zine");
+        break;
+    case "makeco":
+    case "makeco.wpengine.com":
+    case "makeco.staging.wpengine.com/":
+    case "makeco.com":
+        universalNavActive("make");
+        break;
+    case "makershed.com":
+        universalNavActive("shed")
+        break;  
+    case "maker-faire":
+    case "makerfaire":
+    case "makerfaire.wpengine.com":
+    case "makerfaire.staging.wpengine.com":
+    case "makerfaire.com":
+        universalNavActive("faire")
+        break;
+    default:
+          break;
+  }
+  switch(shareSection) {
+    case "maker-share/learning":
+    case "makershare/learning":
+    case "makeshare.wpengine.com/learning":
+    case "makershare.staging.wpengine.com/learning":
+    case "makershare.com/learning":
+        universalNavActive("share")
+        break;
+    case "maker-share/makers":
+    case "makershare/makers":
+    case "makeshare.wpengine.com/makers":
+    case "makershare.staging.wpengine.com/makers":
+    case "makershare.com/makers":
+        universalNavActive("share-p")
+        break;
+    default:
+          break;
+  }
+    
+})(jQuery);
