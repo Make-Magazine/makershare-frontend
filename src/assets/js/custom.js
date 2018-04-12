3b33704809209ed96050c9957633d19433ac87df
Rio Roth-Barrerio <rio@makermedia.com>
2018-04-12 10:20:36 -0700

    Adjust the flyout menu and the sticky nav

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -425,8 +425,7 @@ $(window).on('load', function () {
   // fix nav to top on scrolldown, stay fixed for transition from mobile to desktop
   var e = $(".universal-nav");
   var hamburger = $(".nav-hamburger");
-  var y_pos = $(".nav-level-2").offset().top;
-  console.log(y_pos);
+  var y_pos = $(".nav-level-2").offset().top; // 75
 
   $(window).on('resize', function(){
       if ($(window).width() < 767) {
@@ -442,9 +441,11 @@ $(window).on('load', function () {
       if(scrollTop > y_pos && $(window).width() > 767){
           e.addClass("main-nav-scrolled"); 
           hamburger.addClass("ham-menu-animate");
+          $(".main-container").css("margin-top", "55px");
       }else if(scrollTop <= y_pos){
           e.removeClass("main-nav-scrolled"); 
           hamburger.removeClass("ham-menu-animate");
+          $(".main-container").css("margin-top", "0px");
       }
   });
     
@@ -484,12 +485,6 @@ $(window).on('load', function () {
     case "makerfaire.com":
         universalNavActive("faire")
         break;
-    /*case "maker-share":
-    case "makershare":
-    case "preview.makershare.com":
-    case "makershare.com":
-        universalNavActive("share-p")
-        break;*/
     default:
         break;
   }
@@ -509,4 +504,126 @@ $(window).on('load', function () {
     default:
         break;
   }
+});
+
+////////////////////////////////////////////////
+//////////////// Auth0.js stuff ////////////////
+////////////////////////////////////////////////
+
+var AUTH0_CLIENT_ID    = '0sR3MQz8ihaSnLstc1dABgENHS5PQR8d';
+var AUTH0_DOMAIN       = 'makermedia.auth0.com';
+var AUTH0_CALLBACK_URL = templateUrl + "/authenticated/";
+var AUTH0_REDIRECT_URL = location.href;
+
+window.addEventListener('load', function() {
+  // buttons and event listeners
+  var loginBtn    = document.getElementById('qsLoginBtn');
+  var logoutBtn   = document.getElementById('qsLogoutBtn');
+  var profileView = document.getElementById('profile-view');
+  //default profile view to hidden
+  loginBtn.style.display    = 'none';
+  profileView.style.display = 'none';
+
+  var userProfile;
+  var webAuth = new auth0.WebAuth({
+    domain: AUTH0_DOMAIN,
+    clientID: AUTH0_CLIENT_ID,
+    redirectUri: AUTH0_CALLBACK_URL,
+    audience: 'https://' + AUTH0_DOMAIN + '/userinfo',
+    responseType: 'token id_token',
+    scope: 'openid profile',
+    leeway: 60
+  });
+
+  loginBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    localStorage.setItem('redirect_to',AUTH0_REDIRECT_URL);
+    webAuth.authorize();
+  });
+
+  logoutBtn.addEventListener('click', logout);
+
+  function setSession(authResult) {
+    // Set the time that the access token will expire at
+    var expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime()
+    );
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+  }
+
+  function logout() {
+    // Remove tokens and expiry time from localStorage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    displayButtons();
+  }
+
+  function isAuthenticated() {
+    // Check whether the current time is past the
+    // access token's expiry time
+    var expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
+
+  function handleAuthentication() {
+    webAuth.parseHash(function(err, authResult) {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        window.location.hash = '';
+        setSession(authResult);
+        loginBtn.style.display = 'none';
+
+        //after login redirect to previous page (after 5 second delay)
+        var redirect_url = localStorage.getItem('redirect_to');
+        setTimeout(function(){location.href=redirect_url} , 3000);
+
+      } else if (err) {
+        console.log(err);
+        alert(
+          'Error: ' + err.error + '. Check the console for further details.'
+        );
+      }
+      displayButtons();
+    });
+  }
+
+  function displayButtons() {
+    if (isAuthenticated()) {
+      loginBtn.style.display = 'none';
+      getProfile();
+      profileView.style.display = 'flex';
+    } else {
+      loginBtn.style.display = 'flex';
+      profileView.style.display = 'none';
+    }
+  }
+
+  function getProfile() {
+    if (!userProfile) {
+      var accessToken = localStorage.getItem('access_token');
+
+      if (!accessToken) {
+        console.log('Access token must exist to fetch profile');
+      }
+
+      webAuth.client.userInfo(accessToken, function(err, profile) {
+        if (profile) {
+          userProfile = profile;
+          displayProfile();
+        }
+      });
+    } else {
+      displayProfile();
+    }
+  }
+
+  function displayProfile() {
+    // display the avatar
+    document.querySelector('#profile-view img').src = userProfile.picture;
+  }
+
+  //handle authentication
+  handleAuthentication();
 });
