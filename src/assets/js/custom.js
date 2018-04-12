3002bb2568ad20f0c8cab78c991ea2148e102583
Rio Roth-Barrerio <rio@makermedia.com>
2018-04-12 13:20:04 -0700

    Change auth0 template location

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -512,7 +512,7 @@ $(window).on('load', function () {
 
 var AUTH0_CLIENT_ID    = '0sR3MQz8ihaSnLstc1dABgENHS5PQR8d';
 var AUTH0_DOMAIN       = 'makermedia.auth0.com';
-var AUTH0_CALLBACK_URL = templateUrl + "/authenticated/";
+var AUTH0_CALLBACK_URL = window.location.hostname + "/authenticated/";
 var AUTH0_REDIRECT_URL = location.href;
 
 window.addEventListener('load', function() {
