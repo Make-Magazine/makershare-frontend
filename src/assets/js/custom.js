6d35d9304c7e4b69970095b84b9399948331de1f
Rio Roth-Barrerio <rio@makermedia.com>
2018-06-14 12:00:47 -0700

    check for mutations

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -414,9 +414,30 @@ $(window).bind('hashchange', function() {
 });
 
 // test if anything in the body is changing?
-$('.main-container').bind("DOMSubtreeModified",function(){
-  alert('changed');
-});
+// Select the node that will be observed for mutations
+var targetNode = $('.main-container');
+
+// Options for the observer (which mutations to observe)
+var config = { attributes: true, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+var callback = function(mutationsList) {
+    console.log("something happened");
+    for(var mutation of mutationsList) {
+        if (mutation.type == 'childList') {
+            console.log('A child node has been added or removed.');
+        }
+    }
+};
+
+// Create an observer instance linked to the callback function
+var observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
+
+// Later, you can stop observing
+observer.disconnect();
                            
 $(window).on('load', function () {
   $('#hamburger-icon, #hamburger-makey, .nav-flyout-underlay').click(function() {
