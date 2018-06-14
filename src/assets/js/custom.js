08cb87ccc2c1d92fa2c5a5435c5b6b75150ae068
Rio Roth-Barrerio <rio@makermedia.com>
2018-06-14 12:09:02 -0700

    Play around with mutation code

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -413,31 +413,33 @@ $(window).bind('hashchange', function() {
     console.log("maybe this loads more often");
 });
 
-// test if anything in the body is changing?
-// Select the node that will be observed for mutations
-var targetNode = $('.main-container');
+$(window).on('load', function () {
+    // test if anything in the body is changing?
+    // Select the node that will be observed for mutations
+    var targetNode = $('.main-container')[0];
 
-// Options for the observer (which mutations to observe)
-var config = { attributes: true, childList: true, subtree: true };
+    // Options for the observer (which mutations to observe)
+    var config = { attributes: true, childList: true, subtree: true };
 
-// Callback function to execute when mutations are observed
-var callback = function(mutationsList) {
-    console.log("something happened");
-    for(var mutation of mutationsList) {
-        if (mutation.type == 'childList') {
-            console.log('A child node has been added or removed.');
+    // Callback function to execute when mutations are observed
+    var callback = function(mutationsList) {
+        console.log("something happened");
+        for(var mutation of mutationsList) {
+            if (mutation.type == 'childList') {
+                console.log('A child node has been added or removed.');
+            }
         }
-    }
-};
+    };
 
-// Create an observer instance linked to the callback function
-var observer = new MutationObserver(callback);
+    // Create an observer instance linked to the callback function
+    var observer = new MutationObserver(callback);
 
-// Start observing the target node for configured mutations
-observer.observe(targetNode, config);
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, config);
 
-// Later, you can stop observing
-observer.disconnect();
+    // Later, you can stop observing
+    observer.disconnect();
+});
                            
 $(window).on('load', function () {
   $('#hamburger-icon, #hamburger-makey, .nav-flyout-underlay').click(function() {
