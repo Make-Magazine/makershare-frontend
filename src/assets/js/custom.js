3146a2554a984dba126f8e7eb6b5873ef849a0b4
Rio Roth-Barrerio <rio@makermedia.com>
2018-06-14 13:10:34 -0700

    try to have a unique class for each page

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -410,12 +410,14 @@ $(window).on('load', function () {
 /////// Navigation and Search ////////
 //////////////////////////////////////
 
+$('body').on("mousedown", "a", function (e) {
+    console.log("a link was pressed");
+});
+
 $(window).on('load', function () {
-    console.log("first things first");
     // test if anything in the body is changing?
     // Select the node that will be observed for mutations
     var targetNode = $('.main-container')[0];
-    console.log(targetNode);
     // Options for the observer (which mutations to observe)
     var config = { attributes: true, childList: true, subtree: true };
 
