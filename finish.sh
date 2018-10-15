c22d1189e456809aa8b6e8efb28ac9f79ab19f39
Stephanie Stokes <stephanie@makermedia.com>
2018-10-15 16:42:49 -0500

    Added the Redirect rule for  www to non www and  modified the links to include dist

--- finish.sh
+++ finish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp build.htaccess dist/.htaccess
-cp 401.html dist/401.html
+#cp 401.html dist/401.html
 
 SOURCE="${BASH_SOURCE[0]}";
 RDIR="$( pwd )"
@@ -9,19 +9,19 @@ echo "Source = $SOURCE";
 echo "Dir = $RDIR";
 
 ## Ensure the links exists
-if [ ! -L "dist/learning" ]; then 
-  if [ "$RDIR" =~ "preview.makershare" ]; then   
-    ln -s /home/jazminel/public_html/preview-learning/_site learning 
+if [ ! -L "dist/learning" ]; then
+  if [ "$RDIR" =~ "preview.makershare" ]; then
+    ln -s /home/jazminel/public_html/preview-learning/_site dist/learning
     echo "Directory Preview";
-  else 
-    ln -s /home/jazminel/public_html/learning/_site learning 
+  else
+    ln -s /home/jazminel/public_html/learning/_site dist/learning
     echo "Directory Makershare";
   fi
 #  echo "Learning does not exists";
 fi
-if [ ! -L "dist/static-assets" ]; then 
-   ln - s assets/static-pages/static-assets/ static-assets
+if [ ! -L "dist/static-assets" ]; then
+   ln -s assets/static-pages/static-assets/ dist/static-assets
 fi
-if [ ! -L "dist/fonts" ]; then 
-   ln - s assets/fonts fonts
+if [ ! -L "dist/fonts" ]; then
+   ln -s assets/fonts dist/fonts
 fi
