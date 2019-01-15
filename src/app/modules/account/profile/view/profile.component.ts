04f0c1d5637eb90f533f46d1be077476a96f73f5
Rio Roth-Barrerio <rio@makermedia.com>
2019-01-15 10:54:30 -0800

    only show portfolio notification for logged in users

--- src/app/modules/account/profile/view/profile.component.ts
+++ src/app/modules/account/profile/view/profile.component.ts
@@ -621,7 +621,7 @@ export class ProfileComponent implements OnInit {
     this.FileName = user.user_photo.substring(
       user.user_photo.lastIndexOf('/') + 1,
     );
-	 if(this.FileName == "profile-default.png") {
+	 if(this.FileName == "profile-default.png" && data.user.uid > 0) {
 		this.notificationBarService.create({
 		  message: 'Please upload a profile photo now to start creating projects.',
 		  type: NotificationType.Warning,
