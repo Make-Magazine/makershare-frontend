c334c2369c76e48f43bc553c167ebbc73629d7c4
Travis Stone <travis@makermedia.com>
2019-02-01 14:21:38 -0800

    Roll back that last change; no effect

--- webpack-client.config.js
+++ webpack-client.config.js
@@ -12,7 +12,7 @@ module.exports = {
     path: path.resolve(path.join(__dirname, 'dist')),
     publicPath: '/'
   },
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   resolve: {
     extensions: ['.ts', '.js', '.json']
   },
