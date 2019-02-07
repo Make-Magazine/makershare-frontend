326d05c3607dc934bd0c76fd30325c104c7b22e1
Travis Stone <travis@makermedia.com>
2019-02-07 14:41:08 -0800

    request for asset requiring logged-in user when there is none - try to send the request to auth0 login instead of homepage

--- src/app/modules/auth0/auth-guard.service.ts
+++ src/app/modules/auth0/auth-guard.service.ts
@@ -8,11 +8,13 @@ import {
 } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { UserService } from '../../core/d7services';
+import { Auth } from '../../../modules/auth0/auth.service';
 
 @Injectable()
 export class AuthGuardService implements CanActivate, CanLoad {
   constructor(
      private userService: UserService, 
+     public auth: Auth,
      private router: Router
    ) {}
 
@@ -25,8 +27,8 @@ export class AuthGuardService implements CanActivate, CanLoad {
           console.error('User logged in is ', data);
           if(!data) {
             console.log('We should redirect to the auth0 login page here I think.... ', ActivatedRouteSnapshot);
-
-            this.router.navigateByUrl('/');
+            auth.Auth0Login()
+            //this.router.navigateByUrl('/');
           }
           observer.next(data);
           observer.complete();
