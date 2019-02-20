9533defac7a1320401cf4126a1656ed64db86400
Rio Roth-Barrerio <rio@makermedia.com>
2019-02-20 14:03:37 -0800

    only settings.ts version in the repo should be the prod one

--- src/app/core/models/application/settings.ts
+++ src/app/core/models/application/settings.ts
@@ -14,16 +14,16 @@ export class Settings {
  // solrPath: string = 'https://preview-manage.makershare.com/solr/stage/';
 
  // Preview settings
- apiHost: string = 'preview-manage.makershare.com';
- appURL: string = 'https://preview.makershare.com';
- callbackURL: string = 'https://preview.makershare.com/authenticate-redirect';
- solrPath: string = 'https://preview-manage.makershare.com/solr/stage/';
+ // apiHost: string = 'preview-manage.makershare.com';
+ // appURL: string = 'https://preview.makershare.com';
+ // callbackURL: string = 'https://preview.makershare.com/authenticate-redirect';
+ // solrPath: string = 'https://preview-manage.makershare.com/solr/stage/';
 
  // PROD settings
- // apiHost: string = 'manage.makershare.com';
- // appURL: string = 'https://makershare.com';
- // callbackURL: string = 'https://makershare.com/authenticate-redirect';
- // solrPath: string = 'https://manage.makershare.com/solr/makershare/';
+ apiHost: string = 'manage.makershare.com';
+ appURL: string = 'https://makershare.com';
+ callbackURL: string = 'https://makershare.com/authenticate-redirect';
+ solrPath: string = 'https://manage.makershare.com/solr/makershare/';
 
 
  apiPort: number;
