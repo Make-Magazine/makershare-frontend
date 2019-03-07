ddc8b8ee653d1e950ef8135847fcbac19bdffcea
Rio Roth-Barrerio <rio@makermedia.com>
2019-03-07 11:04:28 -0800

    typo

--- src/app/modules/auth0/auth.service.ts
+++ src/app/modules/auth0/auth.service.ts
@@ -191,7 +191,7 @@ export class Auth {
 			  // Check if user has errors like being underage, if so log them out (although I don't think they get logged in) and let them know
 			  if(querystringArray[0] == "#error=unauthorized") {
 			    var errorDesc = querystringArray[1].split("=");
-				 alert(errorDesc[1].replace(/%20/g, " ").replace(/%253B/g, ",").replace(/%3B/g, ",");
+				 alert(errorDesc[1].replace(/%20/g, " ").replace(/%253B/g, ",").replace(/%3B/g, ","));
 				 this.logout();
 			 }
         }
